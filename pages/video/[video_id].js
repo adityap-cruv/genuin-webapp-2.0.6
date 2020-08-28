bba0610daa2647abfe37293f6b3750db8ca0883e
Hiren Ganatra <hirenlife@gmail.com>
2020-08-28 20:25:32 +0530

    install link set

--- pages/video/[video_id].js
+++ pages/video/[video_id].js
@@ -3,8 +3,11 @@ import Player from '../player';
 import axios from 'axios';
 const Page = (props) => {
   console.log(props);
+  const router = useRouter()
+  const { video_id } = router.query
+  // console.log('video_id', video_id)
   return (
-    <Player {...props.data} {...props.url} />
+    <Player {...props.data} {...props.url} installUrl={'https://install.begenuin.com/86sn?pid=Genuin&video_id='+video_id} />
   )
 }
 Page.getInitialProps = async ({ query: { video_id } }) => {
