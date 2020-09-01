f13256a9b054bee15961f47c55c3cf8e8eba2a6b
Hiren Ganatra <hirenlife@gmail.com>
2020-09-01 15:40:42 +0530

    share link preview reverted for now

--- components/Layout.js
+++ components/Layout.js
@@ -37,12 +37,12 @@ const Layout = ({ children, title, metaImage, metaImageWidth, metaImageHeight, c
         title: 'Genuin',
         description: description,
         images: [
-          {
-            url: metaImage,
-            width: metaImageWidth,
-            height: metaImageHeight,
-            alt: 'Genuin',
-          },
+          // {
+          //   url: metaImage,
+          //   width: metaImageWidth,
+          //   height: metaImageHeight,
+          //   alt: 'Genuin',
+          // },
           {
             url: content,
             width: 300,
