cafe24f20ed9769e7cb18ea69abdbbb5dfc10019
Hiren Ganatra <hirenlife@gmail.com>
2020-09-23 23:21:50 +0530

    preview image debugging

--- pages/[video_id].js
+++ pages/[video_id].js
@@ -69,7 +69,7 @@ const ShortUrlPage = (props) => {
                 twitter={{
                 handle: '@handle',
                 site: '@site',
-                cardType: 'summary',
+                cardType: 'summary_large_image',
                 }}
             />
             <p>Redirecting...</p>
