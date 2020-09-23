34d9c2c0736773e9a7151b3585f7166b8cde28bb
Hiren Ganatra <hirenlife@gmail.com>
2020-09-23 12:32:42 +0530

    index page for designing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from 'next/router';
 import Link from 'next/link';
 import { Alert } from 'react-bootstrap';
+import './index.css'
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
