cb9a53b6649da25c1baf5129cf72758de9931e8a
kaushal.patel <kaushal.patel@iconflux.com>
2020-12-24 20:00:15 +0530

    image added

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ export default class Home extends React.Component {
     
     let children='Genuin';
     let  title='Genuin';
-     let metaImage='../images/genuin_app_icon.png';
+     let metaImage=require('../images/genuin_app_icon.png');
      let metaImageWidth='48';
      let metaImageHeight='48';
      let content='';
