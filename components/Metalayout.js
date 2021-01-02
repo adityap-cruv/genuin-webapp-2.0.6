5a915a9a847c4353a127400554bbc77022bc3291
kishan.iconflux <mohan.ahuja@iconflux.in>
2021-01-02 19:19:39 +0530

    add favicon image

--- components/Metalayout.js
+++ components/Metalayout.js
@@ -63,7 +63,8 @@ const Layout = ({ children, title, metaImage, metaImageWidth, metaImageHeight, c
       }}
     />
      <NextHead>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" /> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
+    <link rel="shortcut icon" href={require('../images/Genuin_logo_transp.ico')} type="image/x-icon" /> 
     <script src="https:code.jquery.com/jquery-3.4.1min.js"></script>
     </NextHead>    
     <div>
