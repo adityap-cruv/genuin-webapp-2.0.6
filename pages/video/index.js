8b45b8f0062a455007d92979fbbbecdd3135b60c
Hiren Ganatra <hiren.ganatra@iconflux.com>
2021-09-24 16:31:52 +0530

    GV-1842 video share url changes for shorter url

--- pages/video/index.js
+++ pages/video/index.js
@@ -7,7 +7,7 @@ const VideoIndex = (props) => {
   const { video_id } = router.query
   // console.log('video_id', video_id)
   return (
-    <Player {...props.data} {...props.url} installUrl={process.env.installurl+video_id} />
+    <Player {...props.data} {...props.url} installUrl={process.env.installurl+props.data.video_id_to_use} />
   )
 }
 VideoIndex.getInitialProps = async ({ query: { video_id } }) => {
@@ -15,8 +15,10 @@ VideoIndex.getInitialProps = async ({ query: { video_id } }) => {
   return axios.get(process.env.apiurl+ "/api/v3/users/video/meta_data/" + video_id)
     .then(response => {
       var resObj = response.data.data;
+      var video_id_to_use = (resObj.video_uuid !== undefined && resObj.video_uuid !== null)?resObj.video_uuid : video_id;
       Object.assign(resObj,{
-        video_id: video_id
+        video_id: video_id,
+        video_id_to_use: video_id_to_use
       })
       return Promise.resolve({ data: resObj})
     })
