1a8cbd0972786171cf61ee5a34cb80cc076bb48c
Hiren Ganatra <hiren.ganatra@iconflux.com>
2021-10-29 15:45:52 +0530

    open graph video url added

--- components/Layout.js
+++ components/Layout.js
@@ -29,6 +29,8 @@ const Layout = ({ children, title, videoUrl, metaImage, metaImageWidth, metaImag
       src: url('/fonts/AvenirNext-Bold-01.ttf');
     }
       `}</style>
+      <meta property="og:video:url" content={videoUrl} />
+      <meta property="og:video:secure_url" content={videoUrl} />
     <NextSeo
       title={title}
       description={description}
