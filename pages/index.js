30e3e02657bec2814031e6bdeefe35359775048f
Himanshu Mendapara <himanshu@begenuin.com>
2023-03-27 15:21:35 +0530

    home nav genuin logo issue resolved

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useBreakpointValue } from "@chakra-ui/react";
 
 
-import { HomeNav } from "../components/homeNav";
+import { HomeNav } from "../components/home_nav";
 import { AnimatedIndexPage } from "../components/animated_index_page";
 import { SimpleIndexPage } from "../components/simple_index_page";
 import { SEO } from "../components/seo";
