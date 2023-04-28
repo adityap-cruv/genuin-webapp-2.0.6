2682a57c33f72dc819e994d051e6f7fcaeb803d0
Himanshu Mendapara <himanshu@begenuin.com>
2023-04-28 14:07:41 +0530

    datadog logs added in footer

--- components/basic/footer.js
+++ components/basic/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Nav, Container, Row, Col } from 'react-bootstrap'
+import { datadogLogs } from '@datadog/browser-logs'
 
 export const Footer = ({
   theme
@@ -26,6 +27,9 @@ export const Footer = ({
                 style={{ opacity: 0.5 }}
                 target="_blank"
                 href={'/content_demo?value=rt_123f373977001407'}
+                onClick = {() => {
+                  datadogLogs.logger.info('Link Clicked', { buttonType: 'Life at Genuin' })
+                }}
               >
                 Life at Genuin
               </Nav.Link>
@@ -44,7 +48,13 @@ export const Footer = ({
               </Nav.Link>
             </Nav.Item>
             <Nav.Item as='li'>
-              <Nav.Link style={{ opacity: 0.5 }} href='/terms'>
+              <Nav.Link
+                style={{ opacity: 0.5 }}
+                href='/terms'
+                onClick={() => {
+                  datadogLogs.logger.info('Link Clicked', { buttonType: 'Terms of Service' })
+                }}
+              >
                 Terms of Service
               </Nav.Link>
             </Nav.Item>
@@ -61,7 +71,13 @@ export const Footer = ({
               </Nav.Link>
             </Nav.Item>
             <Nav.Item as='li'>
-              <Nav.Link style={{ opacity: 0.5 }} href='/privacy'>
+              <Nav.Link
+                style={{ opacity: 0.5 }}
+                href='/privacy'
+                onClick={() => {
+                  datadogLogs.logger.log('Link Clicked', { buttonType: 'Privacy Policy' })
+                }}
+              >
                 Privacy Policy
               </Nav.Link>
             </Nav.Item>
