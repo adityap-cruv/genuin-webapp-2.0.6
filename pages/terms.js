d55761b02b50d01aa7f2ca7c15777f71e96a037d
Himanshu Mendapara <himanshu@begenuin.com>
2023-05-02 12:28:44 +0530

    GV-3840

--- pages/terms.js
+++ pages/terms.js
@@ -15,16 +15,19 @@ const Terms = () => {
   const mainTitleStyle = {
     fontSize: '40px',
     fontWeight: '700',
-    lineHeight: '32px',
+    lineHeight: '45px',
     display: 'flex',
     justifyContent: 'center',
     paddingTop: '40px',
-    paddingBottom: '20px'
+    paddingBottom: '20px',
+    textAlign: 'center'
   }
 
   const generalStyle = {
     fontSize: '16px',
-    lineHeight: '24px'
+    lineHeight: '24px',
+    paddingLeft: '8px',
+    paddingRight: '8px'
   }
 
   const titleStyle = {
@@ -52,8 +55,8 @@ const Terms = () => {
       </section>
       <section className='bg-body h-100'>
         <Container className='mt-3'>
-          <p style={mainTitleStyle}>
-            Terms of Service
+          <p style={{ ...mainTitleStyle }}>
+            Genuin, Inc. <br/>General Terms of Service
           </p>
 
           <div style={generalStyle}>
@@ -370,7 +373,7 @@ const Terms = () => {
                       Genuin, then you agree to set forth the basis of such claim in writing in a "<b>Notice of
                         Claim</b>," as a form of prior notice to Genuin. The Notice of Claim must: (1) describe the nature
                       and basis of the claim or dispute; (2) set forth the specific relief sought; and (3) include your Genuin
-                      account phone number. The Notice of Claim should be submitted to <a
+                      account phone number. The Notice of Claim should be submitted to <a style={{ color: 'blue !important', textDecoration: 'underline' }}
                       href="&#x6d;&#97;&#x69;&#x6c;&#116;&#111;&#x3a;&#99;&#111;&#x6e;&#116;&#97;&#99;&#x74;&#x40;&#98;&#101;&#x67;&#101;&#x6e;&#117;&#x69;&#x6e;&#x2e;&#x63;&#x6f;&#x6d;">&#99;&#111;&#x6e;&#116;&#97;&#99;&#x74;&#x40;&#98;&#101;&#x67;&#101;&#x6e;&#117;&#x69;&#x6e;&#x2e;&#x63;&#x6f;&#x6d;</a>.
                       After you have provided the Notice of Claim to Genuin, the dispute referenced in the Notice of Claim may be
                       submitted by either Genuin or you to arbitration in accordance with Subsection (b) of this Section, below.
