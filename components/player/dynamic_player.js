b9b83d4f6c1602b8426919ee14f75425a1c4ce29
Jimit Shah <jimit@begenuin.com>
2023-05-22 09:32:29 +0530

    replaced genuin-player for testing

--- components/player/dynamic_player.js
+++ components/player/dynamic_player.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import GenuinPlayer from 'genuin_player'
+import React, { useState, useEffect } from 'react'
+import ReactPlayer from 'react-player/lazy'
+import SafariPlayer from './safari_player'
+// import GenuinPlayer from 'genuin_player'
+import isSafari from 'react-device-detect'
 
 export const DynamicPlayer = ({
   isPlaying,
@@ -14,23 +17,23 @@ export const DynamicPlayer = ({
   loop = true,
   onPlaying
 }) => {
-  // const [inViewPort, setInViewPort] = useState(false)
+  const [inViewPort, setInViewPort] = useState(false)
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(entries => {
-  //     entries.forEach(entry => {
-  //       if (entry.intersectionRatio > 0) {
-  //         setInViewPort(true)
-  //       } else {
-  //         setInViewPort(false)
-  //       }
-  //     })
-  //   })
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          setInViewPort(true)
+        } else {
+          setInViewPort(false)
+        }
+      })
+    })
 
-  //   observer.observe(document.body)
-  // }, [])
+    observer.observe(document.body)
+  }, [])
   return (<>
-    <GenuinPlayer
+    {/* <GenuinPlayer
       src={url}
       uniqueKey={uniqueKey}
       controls={false}
@@ -45,8 +48,8 @@ export const DynamicPlayer = ({
       onError={() => console.log('error')}
       onPlaying={onPlaying}
       onTimeUpdate={onProgress}
-    />
-    {/* {!isSafari && <ReactPlayer
+    /> */}
+    {!isSafari && <ReactPlayer
       key={uniqueKey}
       playing={isPlaying && inViewPort}
       url={url}
@@ -81,6 +84,6 @@ export const DynamicPlayer = ({
       onClick={onClick}
       onReady={onReady}
       onPlaying={onPlaying}
-    />} */}
+    />}
   </>)
 }
