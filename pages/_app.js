cece31cc20f5dd0743617997a9ab9622b373ee75
Himanshu <himanshu@begenuin.com>
2023-05-26 17:58:14 +0530

    Genuin player changed to v1.0.3

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 import { ToastContainer } from 'react-toastify'
@@ -7,6 +7,7 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import favicon from '../assets/images/favicon.ico'
 import NextHead from 'next/head'
 import { BasicColors } from '../constants/colors'
+import { initializeApp } from 'firebase/app'
 const theme = extendTheme({
   fonts: {
     body: 'Avenir Next, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji'
@@ -14,6 +15,19 @@ const theme = extendTheme({
 })
 
 function MyApp ({ Component, pageProps }) {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAzKcl1IylxODAtwM8XkVWvpFwzgMEskBs',
+    authDomain: 'genuin-c148b.firebaseapp.com',
+    databaseURL: 'https://genuin-c148b.firebaseio.com',
+    projectId: 'genuin-c148b',
+    storageBucket: 'genuin-c148b.appspot.com',
+    messagingSenderId: '948935706940',
+    appId: '1:948935706940:web:80b617b9783f2d0d51b639',
+    measurementId: 'G-207GT5P81F'
+  }
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig)
+  }, [])
   return (
     <>
       <NextHead>
