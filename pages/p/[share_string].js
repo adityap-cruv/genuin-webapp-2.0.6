1b9f92ffb6ec6779a292b955bceb57e4468945ba
Himanshu Mendapra <himanshu@begenuin.com>
2023-06-06 14:54:14 +0530

    testing fake <a> tag

--- pages/p/[share_string].js
+++ pages/p/[share_string].js
@@ -710,7 +710,6 @@ const Profile = ({
                             h={8}
                             px={5}
                             onClick={() => {
-                              // eslint-disable-next-line no-constant-condition
                               if (isMobile) {
                                 generateDeepLink({
                                   action: 'dm',
@@ -724,7 +723,12 @@ const Profile = ({
                                   utmMedium: 'web',
                                   utmSource: deepLinkParamsRef.current.hostName,
                                   fromUserName: deepLinkParamsRef.current.geshc
-                                }).then(link => window.open(link))
+                                }).then(link => {
+                                  const element = document.createElement('a')
+                                  element.setAttribute('href', link)
+                                  element.target = '_blank'
+                                  element.click()
+                                })
                                   .catch(e => window.open(process.env.hostname))
                               } else {
                                 showGetAppToSendMessage()
