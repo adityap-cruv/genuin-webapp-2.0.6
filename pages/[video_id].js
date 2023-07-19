21fcd953f16e9c3571717dae00273aa1851107b7
Himanshu Mendapra <himanshu@begenuin.com>
2023-07-19 18:48:30 +0530

    package lock update

--- pages/[video_id].js
+++ pages/[video_id].js
@@ -188,7 +188,6 @@ Video.getInitialProps = async ({ query: { video_id, geshc } }) => {
     .then((response) => {
       const resObj = response.data.data
       const video_id_to_use = resObj.share_string
-      console.log('res:::::', resObj)
       Object.assign(resObj, {
         video_id,
         video_id_to_use,
