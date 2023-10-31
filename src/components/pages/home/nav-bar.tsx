47f80eea91f98cb13c0e5906fe3f6bf22ca02caf
Dev Tejot <devtejot@begenuin.com>
2023-10-31 16:24:29 +0530

    modal implementation

--- src/components/pages/home/nav-bar.tsx
+++ src/components/pages/home/nav-bar.tsx
@@ -6,6 +6,11 @@ import { HamBurgerMenuIcon } from '@components/ui/ham-burger'
 import Link from 'next/link'
 import { Sheet, SheetContent, SheetTrigger, SheetClose } from '@components/ui/sheet'
 import { PATH_NAME } from '@lib/utils/constants/path'
+import { Dialog, DialogContent, DialogTrigger } from '@components/ui/dialog'
+import Image from 'next/image'
+import imageAppStore from '@images/appStore.svg'
+import imagePlayStore from '@images/playStore.svg'
+import { DownloadAppForm } from './download-app-form'
 
 export function NavBar() {
   // const navRef = useRef<HTMLElement>(null)
@@ -40,9 +45,26 @@ export function NavBar() {
               size="index-page">
               <p className="text-new-sm">We're hiring!</p>
             </Button>
-            <Button size="index-page" className="bg-new-off-black hover:bg-new-dark-grey after:bg-new-dark-grey">
-              <p className="text-new-off-white text-new-sm">Download Genuin</p>
-            </Button>
+
+            <Dialog>
+              <DialogTrigger>
+                <Button size="index-page" className="bg-new-off-black hover:bg-new-dark-grey after:bg-new-dark-grey">
+                  <p className="text-new-off-white text-new-sm">Download Genuin</p>
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <div className="m-8 text-center w-80">
+                  <h3 className="text-new-h3">Download <br/>Genuin</h3>
+                  <p className="text-new-para-1 m-4">Send the download link to <br/> your phone & email</p>
+                  <DownloadAppForm/>
+                  <p className="text-new-para-2-mobile mt-4 text-new-dark-grey">By clicking Send Link, I acknowledge that I have read the<br/> <a href='/privacy'>Privacy Policy</a> and agree to the <a href='/terms'>Terms of Service</a></p>
+                  <div className="mt-6 flex">
+                    <Image className="mx-2 w-full" src={imageAppStore} alt="app store" />
+                    <Image className="mx-2 w-full" src={imagePlayStore} alt="play store" />
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </nav>
