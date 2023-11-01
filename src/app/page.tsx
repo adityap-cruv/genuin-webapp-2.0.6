56aa6172bb17320186140fb3d141a24e85b727a9
Himanshu Mendapra <himanshu@begenuin.com>
2023-11-01 16:51:06 +0530

    background color fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { cookies } from 'next/headers'
 export default async function Page() {
   const isMobile = cookies().get('mobile')?.value === 'true'
   return (
-    <main className="absolute inset-0 min-h-full w-full text-new-off-black [&>div]:bg-new-off-white">
+    <main className="bg-new-off-white text-new-off-black">
       <NavBar />
       <MainComponent isMobile={isMobile} />
       <Footer />
