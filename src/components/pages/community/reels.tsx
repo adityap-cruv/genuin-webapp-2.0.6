919360ab180e3d1ae74e03e57d33dc263524a945
Jimit <jimit@newslaw.in>
2023-11-02 18:58:55 +0530

    build failed corrected

--- src/components/pages/community/reels.tsx
+++ src/components/pages/community/reels.tsx
@@ -2,7 +2,7 @@
 import { Loader } from '@components/ui/loader'
 import dynamic from 'next/dynamic'
 import { getCommunityVideos } from '@lib/api/community'
-import { useRef } from 'react'
+import { Key, useRef } from 'react'
 
 const Player = dynamic(() => import('@components/common/player').then((comp) => comp.default), {
   loading: (state) => {
@@ -32,7 +32,13 @@ export function CommunityReels({ communityHandle }: CommunityReelsProps) {
         {videos?.length === 0 && <NoReelsAvailable />}
         {isFetched &&
           videos &&
-          videos.map((video, index) => {
+          videos.map((video: {
+              video: { id: string; url: string; share_string: string; description?: string | null | undefined; link?: string | null | undefined; created_at?: string | undefined; updated_at?: string | undefined; thumbnail?: string | undefined; metadata?: { type: string; duration?: number | null | undefined; height?: number | null | undefined; width?: number | null | undefined } | undefined; view_count?: number | null | undefined; reply_count?: number | null | undefined }; owner: {
+                nickname: string; is_avatar:
+                  //todo create error in api component
+                  boolean; profile_image?: string | undefined; username?: string | undefined
+              }; video_type?: "rt" | "public_video" | undefined; loop?: { share_string: string; name?: string | undefined; description?: string | null | undefined; profile_image?: string | null | undefined; discoverable?: boolean | undefined; preview_image?: string | null | undefined } | undefined
+            } | undefined, index: Key | null | undefined) => {
             return (
               <Player
                 key={index}
