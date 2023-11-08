8d94b0b0cb3cf3caf0e5f9beecc28a7b2787de5e
Jimit <jimit@newslaw.in>
2023-11-08 18:07:56 +0530

    meta details added

--- src/app/(site)/c/[handle]/page.tsx
+++ src/app/(site)/c/[handle]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchCommunityDetails } from '@lib/api/community'
 import { cookies } from 'next/headers'
 import { MainComponent } from './main-component'
+import { Metadata } from 'next'
 
 interface Props {
   params: {
@@ -14,4 +15,21 @@ export default async function Component({ params }: Props) {
   console.log("community", communityData)
   const mobileCookie = cookies().get('mobile')?.value
   return <MainComponent communityDetails={communityData} isMobile={mobileCookie === 'true'} />
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const communityData = await fetchCommunityDetails(params.handle)
+  const title = `${communityData.info.name}`
+  let desc = `${communityData.info.description}`
+
+  return {
+    title: title,
+    applicationName: 'Genuin',
+    description: desc,
+    openGraph: {
+      title: title,
+      description: desc,
+      url: `${process.env.NEXT_PUBLIC_HOST_URL}/c/${params.handle}`,
+    },
+  }
 }
--- src/app/(site)/q/[id]/page.tsx
+++ src/app/(site)/q/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'
 import { MainComponent } from './main-component'
 import { fetchQuestionDetails } from '@lib/api/question'
 
@@ -11,4 +12,26 @@ interface Props {
 export default async function Component({ params }: Props) {
   const questionDetails = await fetchQuestionDetails(params.id)
   return <MainComponent questionDetails={questionDetails} />
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const questionDetails = await fetchQuestionDetails(params.id)
+  const title = `Answer '${questionDetails.question}' on Genuin | Reach billions of people with your expert advice.`
+  const desc = 'Answer this trending question on Genuin'
+
+  return {
+    title: title,
+    applicationName: 'Genuin',
+    description: desc,
+    openGraph: {
+      title: title,
+      description: desc,
+      url: `${process.env.NEXT_PUBLIC_HOST_URL}/q/${questionDetails.question_id}`,
+      images: [
+        {
+          url: questionDetails.preview_image
+        },
+      ],
+    },
+  }
 }
