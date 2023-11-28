65509466dad48107060ed86419c5b049f02ac001
Jimit <jimit@newslaw.in>
2023-11-28 17:35:48 +0530

    handled empty description

--- src/app/(site)/c/[handle]/page.tsx
+++ src/app/(site)/c/[handle]/page.tsx
@@ -33,7 +33,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title,
     applicationName: 'Genuin',
-    description: desc,
+    description: desc || '',
     openGraph: {
       title,
       description: desc,
