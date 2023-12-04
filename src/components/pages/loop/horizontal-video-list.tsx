fe713cb7b04266675a0ffcb3bb734c8a119d78ac
Himanshu Mendapra <himanshu@begenuin.com>
2023-12-04 11:45:37 +0530

    GV-5495 fixed

--- src/components/pages/loop/horizontal-video-list.tsx
+++ src/components/pages/loop/horizontal-video-list.tsx
@@ -2,7 +2,6 @@ import { useRef, useState } from 'react'
 import { useScroll, useMotionValueEvent } from 'framer-motion'
 import { getLoopVideos } from '@lib/api/loop'
 import { Loader } from '@components/ui/loader'
-import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import { pushUrlWithoutReload } from '@lib/utils'
 import { PATH_NAME } from '@lib/utils/constants/path'
@@ -24,61 +23,57 @@ export function HorizontalVideosList({ loopId }: { loopId: string }) {
     }
   })
 
-  if (videosDetailsList)
-    return (
-      <div className="relative h-40 w-full">
-        <div className="h-full w-full overflow-y-hidden overflow-x-scroll scroll-smooth whitespace-nowrap" ref={divRef}>
-          {isLoading && <Loader size="md" />}
-          {videosDetailsList?.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="relative mx-1 inline-block cursor-pointer duration-300 hover:scale-95"
-                style={{
-                  height: divRef.current?.getBoundingClientRect().height,
-                  width: (divRef.current?.getBoundingClientRect().height ?? 0) * (9 / 16),
-                }}
-                onClick={(e) => {
-                  pushUrlWithoutReload({
-                    pathname: PATH_NAME.video(item.video.share_string),
-                    query: [{ key: 'l', value: item.loop?.share_string }],
-                  })
-                  setModalState({ isOpen: true, clickedIndex: index })
-                }}>
-                <Image
+  return (
+    <div className="block h-60 min-h-max w-full">
+      <div className="h-full w-full overflow-y-hidden overflow-x-scroll scroll-smooth whitespace-nowrap" ref={divRef}>
+        {isLoading && <Loader size="md" />}
+        {videosDetailsList?.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="relative mx-1 inline-block h-full cursor-pointer duration-300 hover:scale-95"
+              onClick={(e) => {
+                pushUrlWithoutReload({
+                  pathname: PATH_NAME.video(item.video.share_string),
+                  query: [{ key: 'l', value: item.loop?.share_string }],
+                })
+                setModalState({ isOpen: true, clickedIndex: index })
+              }}>
+              <img src={item.video.thumbnail} className="h-full" />
+              {/* <Image
                   src={item.video.thumbnail ?? ''}
                   alt={item.video.description ?? ''}
                   className="object-fill"
                   fill
-                />
-                <p className="absolute left-1 top-1 text-title-sm text-monochrome-white">
-                  {item.video?.metadata?.duration + 's'}
-                </p>
-              </div>
-            )
-          })}
-          {modalState.isOpen && (
-            <PlayerListModal
-              startIndex={modalState.clickedIndex}
-              videosData={videosDetailsList}
-              onOpenChange={(open) => {
-                setModalState((x) => {
-                  x.isOpen = open
-                  return { ...x }
-                })
-              }}
-              closeModal={() => {
-                setModalState((x) => {
-                  x.isOpen = false
-                  return { ...x }
-                })
-              }}
-            />
-          )}
-        </div>
-        {hasNextPage && (
-          <div className="absolute right-0 top-0 z-10 h-full w-24 bg-gradient-to-l from-monochrome-white to-transparent opacity-90" />
+                /> */}
+              <p className="absolute left-1 top-1 text-title-sm text-monochrome-white">
+                {item.video?.metadata?.duration + 's'}
+              </p>
+            </div>
+          )
+        })}
+        {modalState.isOpen && videosDetailsList && (
+          <PlayerListModal
+            startIndex={modalState.clickedIndex}
+            videosData={videosDetailsList}
+            onOpenChange={(open) => {
+              setModalState((x) => {
+                x.isOpen = open
+                return { ...x }
+              })
+            }}
+            closeModal={() => {
+              setModalState((x) => {
+                x.isOpen = false
+                return { ...x }
+              })
+            }}
+          />
         )}
       </div>
-    )
+      {hasNextPage && (
+        <div className="absolute right-0 top-0 z-10 h-full w-24 bg-gradient-to-l from-monochrome-white to-transparent opacity-90" />
+      )}
+    </div>
+  )
 }
