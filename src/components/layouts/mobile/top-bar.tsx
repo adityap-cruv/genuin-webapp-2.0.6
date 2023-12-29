2dc152abdb1cc9627a01e17e5fe670fcd99b8350
Himanshu Mendapra <himanshu@begenuin.com>
2023-12-29 16:51:57 +0530

    bug fixes

--- src/components/layouts/mobile/top-bar.tsx
+++ src/components/layouts/mobile/top-bar.tsx
@@ -88,7 +88,7 @@ function Menu({ hamBurgerVariant = 'dark' }: { hamBurgerVariant: 'dark' | 'light
       <SheetTrigger>
         <HamBurgerMenuIcon toggleToClose={false} variant={hamBurgerVariant} />
       </SheetTrigger>
-      <SheetContent showDefaultClose={false} side="left" className="w-full border-none">
+      <SheetContent showDefaultClose={false} side="left" className="w-full border-none shadow-none outline-none">
         <SheetClose className="shadow-none outline-none">
           <X strokeWidth="3px" className="h-6 w-6 stroke-new-off-black" />
         </SheetClose>
