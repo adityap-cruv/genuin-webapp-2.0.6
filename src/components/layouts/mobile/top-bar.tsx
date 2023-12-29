23f7583680b960385f1838bdcb27de83fad991bf
Himanshu Mendapra <himanshu@begenuin.com>
2023-12-29 16:50:19 +0530

    GV-5648 solved

--- src/components/layouts/mobile/top-bar.tsx
+++ src/components/layouts/mobile/top-bar.tsx
@@ -89,7 +89,7 @@ function Menu({ hamBurgerVariant = 'dark' }: { hamBurgerVariant: 'dark' | 'light
         <HamBurgerMenuIcon toggleToClose={false} variant={hamBurgerVariant} />
       </SheetTrigger>
       <SheetContent showDefaultClose={false} side="left" className="w-full border-none">
-        <SheetClose>
+        <SheetClose className="shadow-none outline-none">
           <X strokeWidth="3px" className="h-6 w-6 stroke-new-off-black" />
         </SheetClose>
         <div className="flex h-full flex-col justify-between pb-5">
