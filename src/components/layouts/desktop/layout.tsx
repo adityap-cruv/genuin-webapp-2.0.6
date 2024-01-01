778d0a8478a8d7217742732beb32d11610a5b63a
Himanshu Mendapra <himanshu@begenuin.com>
2024-01-01 17:30:50 +0530

    bug fixes

--- src/components/layouts/desktop/layout.tsx
+++ src/components/layouts/desktop/layout.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { TopBar } from './top-bar'
 import { SideBar } from './side-bar'
-import { cookies } from 'next/headers'
+import { useSearchParams } from 'next/navigation'
 
 export function Layout(props: any) {
-  const showTopbar = cookies().get('embed')?.value === '1'
+  // TODO: Remove this code.
+  const showTopbar = useSearchParams().get('embed') !== '1'
+
   return (
     <main className="absolute inset-0 flex h-full min-h-max w-full flex-col items-center overflow-clip">
       {!showTopbar && <TopBar />}
