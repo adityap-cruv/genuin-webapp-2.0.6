6a8675ceabce52c8e435d94bd97f4f13c54e218d
Himanshu Mendapra <himanshu@begenuin.com>
2024-01-03 19:43:47 +0530

    EOD commit

--- src/app/(site)/@desktop/profile/[nickname]/main-component.tsx
+++ src/app/(site)/@desktop/profile/[nickname]/main-component.tsx
@@ -17,7 +17,6 @@ import Link from 'next/link'
 import { PATH_NAME } from '@lib/utils/constants/path'
 import { useInView, useMotionValueEvent, useScroll } from 'framer-motion'
 import { TopBar } from './top-bar'
-import { PlayerModal } from '@components/common/player-modal'
 
 interface CompProps {
   profileData: any
@@ -129,34 +128,28 @@ function CommunityList({ usernickname }: any) {
   return (
     <div ref={scrollDivRef} className="h-full w-full overflow-y-auto">
       {isLoading && <Loader size="md" />}
-      {data && (
-        <div>
-          {communities?.map((item, index) => (
-            <div key={index} className="my-6">
-              <div className="flex items-center">
-                <CustomAvatar
-                  className="bg-slate-500 h-11 w-11 bg-red-40"
-                  fallbackString={item?.name}
-                  imageUrl={item?.dp}
-                  isAvatar={false}
-                />
-                <Link href={{ pathname: PATH_NAME.community(item.handle) }}>
-                  <div className="mx-2">
-                    <p
-                      className="line-clamp-1 text-left"
-                      style={{ fontWeight: 600, fontSize: '20px', lineHeight: '24px' }}>
-                      {item.name}
-                    </p>
-                  </div>
-                </Link>
+      {communities?.map((item, index) => (
+        <div key={index} className="my-6">
+          <div className="flex items-center">
+            <CustomAvatar
+              className="bg-slate-500 h-11 w-11 bg-red-40"
+              fallbackString={item?.name}
+              imageUrl={item?.dp}
+              isAvatar={false}
+            />
+            <Link href={{ pathname: PATH_NAME.community(item.handle) }}>
+              <div className="mx-2">
+                <p className="line-clamp-1 text-left" style={{ fontWeight: 600, fontSize: '20px', lineHeight: '24px' }}>
+                  {item.name}
+                </p>
               </div>
-              <DecorativeList>
-                <CommunityDetails userId={usernickname} communityHandle={item.handle} />
-              </DecorativeList>
-            </div>
-          ))}
+            </Link>
+          </div>
+          <DecorativeList>
+            <CommunityDetails userId={usernickname} communityHandle={item.handle} />
+          </DecorativeList>
         </div>
-      )}
+      ))}
       {isFetchingNextPage && <Loader size="md" />}
     </div>
   )
@@ -164,29 +157,32 @@ function CommunityList({ usernickname }: any) {
 
 function CommunityDetails({ userId, communityHandle }: { userId: string; communityHandle: string }) {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = getAllLoops(userId, communityHandle)
+  const loops = data?.pages.flatMap((item) => item.loops)
   const handleSeeMoreClick = () => {
     void fetchNextPage()
   }
-  return (
-    <>
-      &nbsp;
-      {isLoading && (
-        <li
-          className="profile-loop-li relative my-4 w-full rounded-lg border border-monochrome-9 p-4"
-          style={{ backgroundColor: '#F9F9F9' }}>
-          <Shimmer className="h-6 w-40" />
-          <div className="my-2 grid w-full grid-cols-4 gap-2 md:grid-cols-8">
-            {Array.from({ length: 8 }).map((_, index) => (
-              <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
-                <Shimmer className="aspect-reel w-full rounded" />
-              </div>
-            ))}
-          </div>
-        </li>
-      )}
-      {data?.pages
-        .flatMap((page) => page.loops)
-        .map((item: any, index: any) => (
+
+  if (isLoading)
+    return (
+      <li
+        className="profile-loop-li relative my-4 w-full rounded-lg border border-monochrome-9 p-4"
+        style={{ backgroundColor: '#F9F9F9' }}>
+        <Shimmer className="h-6 w-40" />
+        <div className="my-2 grid w-full grid-cols-4 gap-2 md:grid-cols-8">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
+              <Shimmer className="aspect-reel w-full rounded" />
+            </div>
+          ))}
+        </div>
+      </li>
+    )
+
+  if (loops)
+    return (
+      <>
+        <div className="h-1 w-full" />
+        {loops?.map((item: any, index: any) => (
           <li
             className="profile-loop-li relative my-4 w-full rounded-lg border border-monochrome-9 p-4 pb-2"
             style={{ backgroundColor: '#F9F9F9' }}
@@ -194,22 +190,23 @@ function CommunityDetails({ userId, communityHandle }: { userId: string; communi
             <LoopVideos userId={userId} loopDetails={item} />
           </li>
         ))}
-      {isFetchingNextPage && <Loader size="md" />}
-      {hasNextPage && (
-        <p
-          className="text-blue-500 flex w-full cursor-pointer justify-center pt-2 text-cap-lg text-monochrome"
-          onClick={handleSeeMoreClick}>
-          See More Loops
-        </p>
-      )}
-    </>
-  )
+        {isFetchingNextPage && <Loader size="md" />}
+        {hasNextPage && (
+          <p
+            className="text-blue-500 flex w-full cursor-pointer justify-center pt-2 text-cap-lg text-monochrome"
+            onClick={handleSeeMoreClick}>
+            See More Loops
+          </p>
+        )}
+      </>
+    )
 }
 
 type LoopVideosProps = {
   userId: string
   loopDetails: any
 }
+
 function LoopVideos({ userId, loopDetails }: LoopVideosProps) {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = getAllLoopVideos(userId, loopDetails.slug)
   const videos = data?.pages.flatMap((item) => item.videos)
@@ -223,6 +220,40 @@ function LoopVideos({ userId, loopDetails }: LoopVideosProps) {
     }
   }
 
+  function InnerComponent() {
+    if (isLoading)
+      return Array.from({ length: 8 }).map((_, index) => (
+        <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
+          <Shimmer className="aspect-reel w-full rounded" />
+        </div>
+      ))
+
+    if (videos && videos.length === 0)
+      return (
+        <div className="flex items-center justify-center pt-32 text-title-md text-secondary">No videos available</div>
+      )
+    if (videos)
+      return videos?.map((video, index) => (
+        <>
+          <div key={video.id} className="relative flex aspect-reel min-w-full flex-col items-center">
+            <img src={video.thumbnail} alt={`Video Thumbnail ${index}`} className="aspect-reel rounded" />
+            <div className="absolute bottom-0 left-0 m-1 flex items-center justify-center">
+              <Image src={icSpark} alt="share" height={15} width={15} />
+              <p className="text-new-para-2-mobile text-monochrome-white">
+                {abbreviateNumber(video.no_of_sparks) || 0}
+              </p>
+            </div>
+          </div>
+          {isFetchingNextPage &&
+            Array.from({ length: Math.max(0, loopDetails.video_count - videoCount) }).map((_, index) => (
+              <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
+                <Shimmer className="aspect-reel h-full rounded" />
+              </div>
+            ))}
+        </>
+      ))
+  }
+
   return (
     <>
       <Link href={{ pathname: PATH_NAME.loop(loopDetails.slug) }}>
@@ -230,35 +261,8 @@ function LoopVideos({ userId, loopDetails }: LoopVideosProps) {
           {loopDetails.name}
         </p>
       </Link>
-      {videos?.length === 0 && (
-        <div className="flex items-center justify-center pt-32 text-title-md text-secondary">No videos available</div>
-      )}
       <div className="my-2 grid w-full grid-cols-4 gap-2 md:grid-cols-8">
-        {isLoading &&
-          Array.from({ length: 8 }).map((_, index) => (
-            <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
-              <Shimmer className="aspect-reel w-full rounded" />
-            </div>
-          ))}
-        {videos?.map((video, index) => (
-          <PlayerModal videoDetails={video} key={index}>
-            <div key={video.id} className="relative flex aspect-reel min-w-full flex-col items-center">
-              <img src={video.thumbnail} alt={`Video Thumbnail ${index}`} className="aspect-reel rounded" />
-              <div className="absolute bottom-0 left-0 m-1 flex items-center justify-center">
-                <Image src={icSpark} alt="share" height={15} width={15} />
-                <p className="text-new-para-2-mobile text-monochrome-white">
-                  {abbreviateNumber(video.no_of_sparks) || 0}
-                </p>
-              </div>
-            </div>
-          </PlayerModal>
-        ))}
-        {isFetchingNextPage &&
-          Array.from({ length: Math.max(0, loopDetails.video_count - videoCount) }).map((_, index) => (
-            <div key={`shimmer-${index}`} className="relative flex flex-col items-center">
-              <Shimmer className="aspect-reel h-full rounded" />
-            </div>
-          ))}
+        <InnerComponent />
       </div>
       {hasNextPage && (
         <p
