dc8cb3bd5a42cb450d0177372b22a33851c8220a
Himanshu Mendapra <himanshu@begenuin.com>
2024-01-08 15:22:22 +0530

    GV-5705 fixed

--- src/components/common/feed/desktop-details.tsx
+++ src/components/common/feed/desktop-details.tsx
@@ -23,25 +23,105 @@ type DesktopDetailsProps = {
   videoDetails: VideoDataType
 }
 export function DesktopDetails({ videoDetails }: DesktopDetailsProps) {
+  const { shareFn } = useAdaptiveShare()
+  const { toast } = useToast()
   const scrollDivRef = useRef<HTMLDivElement>(null)
 
   if (videoDetails)
     return (
-      <div className="relative flex-1 overflow-x-clip bg-monochrome-white pb-16">
-        <div ref={scrollDivRef} className="flex h-full flex-col overflow-auto overflow-x-clip">
-          <div className="w-full p-6 pb-4">
-            <InfoArea videoDetails={videoDetails} />
+      <div className="relative h-full flex-1 overflow-x-clip bg-monochrome-white pl-2">
+        <div className="border-b border-monochrome-black/10 p-4">
+          <span className="flex items-center gap-x-2">
+            <CustomAvatar
+              isAvatar={videoDetails.owner.is_avatar}
+              fallbackString={videoDetails.owner.username ?? ''}
+              imageUrl={videoDetails.owner.profile_image ?? ''}
+              className="h-9 w-9"
+            />
+            <span className="flex items-center gap-x-1">
+              <Link href={PATH_NAME.profile(videoDetails.owner.nickname)}>
+                <p className="text-body-lg">@{videoDetails.owner.nickname}</p>
+              </Link>
+              <p className="text-body-1-demi text-secondary">{getTimeAgo(videoDetails.video.created_at)}</p>
+            </span>
+          </span>
+          {videoDetails.video.description && (
+            <p className="line-clamp-2 w-5/6 overflow-hidden break-all pt-3 text-title-3-med">
+              {videoDetails.video.description}
+            </p>
+          )}
+        </div>
+        <div ref={scrollDivRef} className="flex h-full flex-col overflow-auto overflow-x-clip pb-16">
+          <div className="p-4">
+            <p className="text-title-3-bold">Posted in</p>
+            <div className="pt-3">
+              <span className="flex items-center justify-between">
+                <span className="flex items-center gap-x-3">
+                  <CustomAvatar
+                    imageUrl={videoDetails.community.dp ?? ''}
+                    fallbackString={videoDetails.community.name ?? ''}
+                    isAvatar={false}
+                    className="h-11 w-11"
+                  />
+                  <Link href={{ pathname: PATH_NAME.community(videoDetails.community.slug) }}>
+                    <p className="line-clamp-1 w-full break-all pr-2 text-title-3-bold">
+                      {videoDetails.community.name}
+                    </p>
+                  </Link>
+                </span>
+                <span className="flex h-min items-center gap-x-3">
+                  <DownloadDialog
+                    title="Get the Genuin app"
+                    subtitle={
+                      <>
+                        Get the app to join the <br />
+                        <span className="font-bold">@{videoDetails.community.handle}</span> community.
+                      </>
+                    }
+                    asChild>
+                    <Button size="custom">
+                      <p className="whitespace-nowrap px-4 py-1 text-title-3-demi">Join Community</p>
+                    </Button>
+                  </DownloadDialog>
+                  <Button
+                    size="custom"
+                    variant="outline"
+                    className="min-w-max border-2 border-primary p-1 "
+                    onClick={async () =>
+                      await shareFn({
+                        shareLink:
+                          window.location.host +
+                          PATH_NAME.community(videoDetails.community.slug) +
+                          '&utm_source=app_web',
+                        toast: () => toast({ title: 'Link Copied!', duration: 1000 }),
+                      })
+                    }>
+                    <Image src={icShare} alt="share" className="h-5 w-5" />
+                  </Button>
+                </span>
+              </span>
+              <DecorativeList>
+                <div className="h-2 w-full" />
+                <a href={PATH_NAME.loop(videoDetails.loop.slug)}>
+                  <li className="relative flex h-full w-full items-center justify-between rounded-md border border-monochrome-9 bg-monochrome-10 p-4 ">
+                    <p className="line-clamp-1 w-full break-all pr-2 text-body-1-demi">{videoDetails.loop?.name}</p>
+                    <p className="whitespace-nowrap text-cap-1-med text-primary">View Loop</p>
+                  </li>
+                </a>
+              </DecorativeList>
+            </div>
           </div>
           <div className="sticky top-0 z-10">
             <p className="border-b border-t border-monochrome-black/10 bg-monochrome-white px-4 py-3 text-title-3-demi text-secondary">
               Comments {videoDetails.video.no_of_comments !== 0 ? `(${videoDetails.video.no_of_comments})` : ''}
             </p>
           </div>
-          <div className="h-full px-6 pb-20 pt-2">
+          <div className="h-full px-4 pt-2">
             <CommentBox shareString={videoDetails.video.share_string} parentRef={scrollDivRef} />
           </div>
         </div>
         <CommentInput />
+        <Toaster />
       </div>
     )
 }
@@ -69,7 +149,7 @@ function CommentBox({ shareString, parentRef }: { shareString: string; parentRef
 
   if (comments && comments?.length !== 0)
     return (
-      <div className="h-full overflow-visible">
+      <div className="h-auto overflow-visible pb-40">
         <Comments.withoutApi
           comments={comments}
           fetchNextPage={() => {}}
@@ -87,92 +167,9 @@ function CommentBox({ shareString, parentRef }: { shareString: string; parentRef
   )
 }
 
-function InfoArea({ videoDetails }: { videoDetails: VideoDataType }) {
-  const { shareFn } = useAdaptiveShare()
-  const { toast } = useToast()
-  return (
-    <>
-      <span className="flex items-center gap-x-2">
-        <CustomAvatar
-          isAvatar={videoDetails.owner.is_avatar}
-          fallbackString={videoDetails.owner.username ?? ''}
-          imageUrl={videoDetails.owner.profile_image ?? ''}
-          className="h-9 w-9"
-        />
-        <span className="flex items-center gap-x-1">
-          <Link href={PATH_NAME.profile(videoDetails.owner.nickname)}>
-            <p className="text-body-lg">@{videoDetails.owner.nickname}</p>
-          </Link>
-          <p className="text-body-1-demi text-secondary">{getTimeAgo(videoDetails.video.created_at)}</p>
-        </span>
-      </span>
-      <div className="pb-6 pt-2">
-        <p className="line-clamp-2 w-5/6 overflow-hidden break-all text-title-3-med">
-          {videoDetails.video.description}
-        </p>
-      </div>
-      <hr className=" border border-monochrome-black/10" />
-      <p className="pt-6 text-title-3-bold">Posted in</p>
-      <div className="pt-3">
-        <span className="flex items-center justify-between">
-          <span className="flex items-center gap-x-3">
-            <CustomAvatar
-              imageUrl={videoDetails.community.dp ?? ''}
-              fallbackString={videoDetails.community.name ?? ''}
-              isAvatar={false}
-              className="h-11 w-11"
-            />
-            <Link href={{ pathname: PATH_NAME.community(videoDetails.community.slug) }}>
-              <p className="line-clamp-1 w-full break-all pr-2 text-title-3-bold">{videoDetails.community.name}</p>
-            </Link>
-          </span>
-          <span className="flex h-min items-center gap-x-3">
-            <DownloadDialog
-              title="Get the Genuin app"
-              subtitle={
-                <>
-                  Get the app to join the <br />
-                  <span className="font-bold">@{videoDetails.community.handle}</span> community.
-                </>
-              }
-              asChild>
-              <Button size="custom">
-                <p className="whitespace-nowrap px-4 py-1 text-title-3-demi">Join Community</p>
-              </Button>
-            </DownloadDialog>
-            <Button
-              size="custom"
-              variant="outline"
-              className="min-w-max border-2 border-primary p-1 "
-              onClick={async () =>
-                await shareFn({
-                  shareLink:
-                    window.location.host + PATH_NAME.community(videoDetails.community.slug) + '&utm_source=app_web',
-                  toast: () => toast({ title: 'Link Copied!', duration: 1000 }),
-                })
-              }>
-              <Image src={icShare} alt="share" className="h-5 w-5" />
-            </Button>
-          </span>
-        </span>
-        <DecorativeList>
-          <div className="h-2 w-full" />
-          <a href={PATH_NAME.loop(videoDetails.loop.slug)}>
-            <li className="relative flex h-full w-full items-center justify-between rounded-md border border-monochrome-9 bg-monochrome-10 p-4 ">
-              <p className="line-clamp-1 w-full break-all pr-2 text-body-1-demi">{videoDetails.loop?.name}</p>
-              <p className="whitespace-nowrap text-cap-1-med text-primary">View Loop</p>
-            </li>
-          </a>
-        </DecorativeList>
-      </div>
-      <Toaster />
-    </>
-  )
-}
-
 function CommentInput() {
   return (
-    <div className="sticky bottom-0 left-0 h-16 w-full border-t-2 border-t-monochrome-9 bg-monochrome-10 py-3 shadow-md">
+    <div className="absolute bottom-0 left-0 h-16 w-full border-t-2 border-t-monochrome-9 bg-monochrome-10 py-3 shadow-md">
       <DownloadDialog title="Get the Genuin app" subtitle="Get the app to comment on this video." asChild>
         <button className="flex w-full flex-1 items-center gap-x-4 pl-6">
           <div
