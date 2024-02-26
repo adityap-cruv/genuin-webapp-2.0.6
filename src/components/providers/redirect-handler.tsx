051a4b42c042d096cc94dd89a9786d80af44af4f
Himanshu Mendapra <himanshu@begenuin.com>
2024-02-26 17:03:19 +0530

    redirection stopped in case of domain

--- src/components/providers/redirect-handler.tsx
+++ src/components/providers/redirect-handler.tsx
@@ -2,8 +2,21 @@ import { type ConfigType } from '@lib/stores/genuin-options'
 import { checkAndAppendHttps } from '@lib/utils'
 import { permanentRedirect } from 'next/navigation'
 
-export function RedirectHandler({ children, config }: { children: React.ReactNode; config?: ConfigType }) {
-  if (config && config?.integrations.white_label.enable && config?.integrations.white_label.allowed_domains[0]) {
+export function RedirectHandler({
+  children,
+  config,
+  shouldRedirect = false,
+}: {
+  children: React.ReactNode
+  config?: ConfigType
+  shouldRedirect: boolean
+}) {
+  if (
+    config &&
+    config?.integrations.white_label.enable &&
+    config?.integrations.white_label.allowed_domains[0] &&
+    shouldRedirect
+  ) {
     permanentRedirect(checkAndAppendHttps(config.integrations.white_label.allowed_domains[0]))
   }
 
