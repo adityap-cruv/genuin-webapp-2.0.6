085da4837a2bdbf10ce17b38046ae6fb1ca36868
Dev Tejot <devtejot@begenuin.com>
2024-02-27 16:57:25 +0530

    pricing count bug fix

--- src/app/(static)/pricing/mobile.tsx
+++ src/app/(static)/pricing/mobile.tsx
@@ -131,7 +131,7 @@ function Component1() {
                 </div>
                 <div className="flex items-center gap-2">
                   <Image priority loading="eager" src={check} width={24} height={24} alt="Star" />
-                  <p className="text-new-para-2-mobile">10,000 MaU included with additional MaUs at $0.5/MaU</p>
+                  <p className="text-new-para-2-mobile">1000 MaU included with additional MaUs at $0.5/MaU</p>
                 </div>
                 <div className="flex items-center gap-2">
                   <Image priority loading="eager" src={exclamation} width={24} height={24} alt="Star" />
