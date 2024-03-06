47e6b68e80ab07d3ef1718f7cee910d08d0e2f55
Dev Tejot <devtejot@begenuin.com>
2024-03-06 11:44:38 +0530

    updated discover page & some design changes

--- src/app/(static)/discover/desktop.tsx
+++ src/app/(static)/discover/desktop.tsx
@@ -42,8 +42,8 @@ function SearchSection() {
         backgroundPosition: 'center',
       }}>
       <div className="container flex flex-col items-center">
-        <p className="mb-6 mt-14 text-center text-new-h2">Find your community on Genuin</p>
-        <div className="relative flex w-3/5 items-center justify-start">
+        <p className="mb-6 mt-24 text-center text-new-h2">Communities on Genuin</p>
+        {/* <div className="relative flex w-3/5 items-center justify-start">
           <img src={search.src} className="absolute left-4" alt="genuin" />
           <input
             type="text"
@@ -52,7 +52,7 @@ function SearchSection() {
             onChange={handleInputChange}
             placeholder=" Search Genuin"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   )
@@ -125,7 +125,7 @@ function LoopSection() {
   return (
     <div className="container my-4 mt-8 px-40">
       <p className=" text-new-para-1">Featured Loops</p>
-      <div className="mt-6 grid grid-cols-2 gap-6">
+      <div className="my-6 grid grid-cols-2 gap-6">
         {loopList.map((item, index) => {
           return (
             <div key={index}>
