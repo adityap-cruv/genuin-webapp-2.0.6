d6dc01853031738677b99e4811ec4fef66fe745c
Himanshu Mendapra <himanshu@begenuin.com>
2024-03-13 12:54:50 +0530

    fix: redirection issues fixed

--- src/app/(static)/verify-email/client-component.tsx
+++ src/app/(static)/verify-email/client-component.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export function ClientComponent({ user, redirectTo }: { user: any; redirectTo: string }) {
+  const router = useRouter()
   useEffect(() => {
     console.log('::signing in::')
-    void signIn('credentials', { ...user, callbackUrl: redirectTo, redirect: true })
+    void signIn('credentials', { ...user, callbackUrl: redirectTo, redirect: false })
       .then((val) => {
         if (val?.ok) {
           console.log('signed in..')
+          router.replace(redirectTo)
         }
       })
       .catch((e) => {
