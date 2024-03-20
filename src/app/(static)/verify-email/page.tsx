72603e89a4642f3a3f23d8df49b2953ecd920492
Himanshu Mendapra <himanshu@begenuin.com>
2024-03-20 17:38:57 +0530

    fix: rediection handled

--- src/app/(static)/verify-email/page.tsx
+++ src/app/(static)/verify-email/page.tsx
@@ -26,7 +26,7 @@ export default async function Page({ searchParams }: { searchParams: { token: st
     redirect(getRedirectTo({ email, emailType, error: false, path: actionMetadata?.path, success: false }))
   }
 
-  // redirect(getRedirectTo({ emailType, error: true, path: actionMetadata?.path }))
+  redirect(getRedirectTo({ emailType, error: true, path: actionMetadata?.path }))
 }
 
 function getRedirectTo({
