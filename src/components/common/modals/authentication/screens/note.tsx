e5ae1c74e61e03ff40a274f6c7796b23c87d6083
Himanshu Mendapra <himanshu@begenuin.com>
2024-03-21 11:29:45 +0530

    fix: removed resend buton

--- src/components/common/modals/authentication/screens/note.tsx
+++ src/components/common/modals/authentication/screens/note.tsx
@@ -12,7 +12,7 @@ export const Note = {
 
 function Email({ acountExists = false }: { acountExists?: boolean }) {
   const email = useAuthenticationModalStore().formData.email
-  const [error, setError] = useState('')
+  const [error, setError] = useState({ message: '', code: 0 })
   const [emailSentText, setEmailSentText] = useState('')
 
   async function resendMail() {
@@ -22,13 +22,17 @@ function Email({ acountExists = false }: { acountExists?: boolean }) {
           if (res.code === 200) {
             setEmailSentText('Email has been sent sucessfully')
           } else if (res.code === 5239) {
-            setError('Email has already been verified')
+            setError((x) => {
+              return { message: 'Email has already been verified', code: res.code }
+            })
           } else {
             throw new Error()
           }
         })
         .catch((e) => {
-          setError('Something went wrong.Please try again.')
+          setError((x) => {
+            return { message: 'Something went wrong.Please try again.', code: -1 }
+          })
         })
   }
 
@@ -39,16 +43,20 @@ function Email({ acountExists = false }: { acountExists?: boolean }) {
         <span className="text-title-1-bold">{shortenedEmail(email)}</span>. Verify your email to save your profile{' '}
         {acountExists && 'and continue'}.
       </p>
-      <p className="text-title-3-demi">
-        Not seeing the email?{' '}
-        <span onClick={resendMail} className="cursor-pointer text-primary">
-          Resend
-        </span>
-      </p>
+      {error.code !== 5239 && (
+        <p className="text-title-3-demi">
+          Not seeing the email?{' '}
+          <span onClick={resendMail} className="cursor-pointer text-primary">
+            Resend
+          </span>
+        </p>
+      )}
       {emailSentText && (
         <p className="flex items-center justify-center text-title-3-med text-supplementary-green">{emailSentText}</p>
       )}
-      {error && <p className="flex items-center justify-center text-title-3-med text-supplementary-red">{error}</p>}
+      {error.message && (
+        <p className="flex items-center justify-center text-title-3-med text-supplementary-red">{error.message}</p>
+      )}
     </ModalShell>
   )
 }
