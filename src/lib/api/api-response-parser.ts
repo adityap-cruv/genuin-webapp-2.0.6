bbd1e781af0789d8dba0fe3dcd5cd607bf1acc85
Jimit Shah <jimit@begenuin.com>
2024-04-18 13:10:37 +0530

    GEN-380

--- src/lib/api/api-response-parser.ts
+++ src/lib/api/api-response-parser.ts
@@ -56,7 +56,7 @@ export function parseFeedResponse(videos: FeedResponseType) {
         profileImage: video.community.dp,
       },
       loop: {
-        id: video.group.group_id,
+        id: video.chat_id,
         slug: video.slug,
         name: video.group.group_name,
       },
