894c7fb4b3be8e1480cdf2a11dd1624ffbafdc42
Jimit Shah <jimit@begenuin.com>
2024-04-23 16:30:06 +0530

    hotfix:multiple reload

--- src/components/providers/genuin-options-provider.tsx
+++ src/components/providers/genuin-options-provider.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from 'react'
 import { useLocalStorage } from '@lib/stores/local-storage'
 import FingerpringJS from '@fingerprintjs/fingerprintjs'
 import { useSession } from 'next-auth/react'
-import {} from '@components/common/modals/download-app'
 import { axiosInstance, setAuthTokenInAxiosInstance } from '@lib/api/instance'
 import { encryptText, parseUserAgent } from '@lib/utils'
 import dynamic from 'next/dynamic'
@@ -26,6 +25,10 @@ type Props = {
   browserType: string
   config?: ConfigType
 }
+
+// it won't log any consoles in production.
+// eslint-disable-next-line no-console
+// if (process.env.NEXT_PUBLIC_CURRENT_ENV === 'prod') console.log = () => {}
 
 // TODO: separate this component into 2 comps with once has auth and second doesn't have auth.
 export function GenuinOptionsProvider({ children, deviceType, os, browserType, config }: Props) {
@@ -59,12 +62,13 @@ export function GenuinOptionsProvider({ children, deviceType, os, browserType, c
     if (sessionStatus === 'authenticated') {
       setAuthTokenInAxiosInstance(sessionData.user.accessToken)
       setInitialData({ user: sessionData.user })
+      if (isLoading) setIsLoading(false)
     }
     if (sessionStatus === 'unauthenticated') {
       setInitialData({ user: undefined })
       setAuthTokenInAxiosInstance(undefined)
+      if (isLoading) setIsLoading(false)
     }
-    setIsLoading(false)
   }, [sessionStatus])
 
   function init() {
@@ -123,27 +127,39 @@ export function GenuinOptionsProvider({ children, deviceType, os, browserType, c
     }
   }, [])
 
-  if (!config) {
-    return (
-      <>
-        {children}
-        <DownloadDialogModal />
-      </>
-    )
-  }
+  console.log('What happening::', isLoading, sessionStatus, sessionData)
+  // if (!config) {
+  //   return (
+  //     <>
+  //       {children}
+  //       <DownloadDialogModal />
+  //       <AuthenticationModal />
+  //     </>
+  //   )
+  // }
+
+  // if (isLoading) return <SplashScreen />
+
+  // if (config) {
+  //   if (sessionStatus === 'loading') return <SplashScreen />
+
+  //   return (
+  //     <>
+  //       {children}
+  //       <DownloadDialogModal />
+  //       <AuthenticationModal />
+  //     </>
+  //   )
+  // }
 
   if (isLoading) return <SplashScreen />
-
-  if (config) {
-    if (sessionStatus === 'loading') return <SplashScreen />
-
-    return (
-      <>
-        {children}
-        <AuthenticationModal />
-      </>
-    )
-  }
+  return (
+    <>
+      {children}
+      {!!config && <AuthenticationModal />}
+      {!config && <DownloadDialogModal />}
+    </>
+  )
 }
 
 // TODO: move it to right location.
