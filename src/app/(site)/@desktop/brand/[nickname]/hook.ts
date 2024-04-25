f8c24d7dd89c2bd1aefc6c5ddbf7557c0823786b
Himanshu <himanshu@begenuin.com>
2024-04-25 13:18:58 +0530

    feat: added brand page (same as profile)

---
+++ src/app/(site)/@desktop/brand/[nickname]/hook.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+export function getNextPage<T>(funcToCall: () => Promise<T>, stateUpdationFunc: (data: T) => void) {
+  const [isFetchingNextPage, setIsFetchingNextPage] = useState(false)
+
+  function fetchNext() {
+    setIsFetchingNextPage(true)
+    funcToCall()
+      .then((res) => {
+        stateUpdationFunc(res)
+      })
+      .catch((e) => {
+        console.log('error in loops:', e)
+      })
+      .finally(() => {
+        setIsFetchingNextPage(false)
+      })
+  }
+
+  return { isFetchingNextPage, fetchNext }
+}
