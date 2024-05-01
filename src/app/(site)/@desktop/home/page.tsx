34dc606e5647abc69fc1efe7eaae9c363c8d51f5
Jimit Shah <jimit@begenuin.com>
2024-05-01 11:35:34 +0530

    handled cases for the mobile view

--- src/app/(site)/@desktop/home/page.tsx
+++ src/app/(site)/@desktop/home/page.tsx
@@ -17,26 +17,22 @@ type HomeMetadata = {
 
 export async function generateMetadata(): Promise<Metadata> {
   const host = headers().get('host') ?? ''
-  console.log("Host::", host)
   const config = getConfig(host)
-  console.log("Config::", config)
   if (config) {
     const metadataParams = { type: 5, ...config }
     const metadata: HomeMetadata = await fetchMetadata(metadataParams)
-    console.log("Metadata Resp:", metadata)
     return {
-      title: metadata.title,
+      title: metadata.title || '',
       // applicationName: 'Genuin',
       description: metadata.description || '',
       openGraph: {
-        title: metadata.title,
-        description: metadata.description,
+        title: metadata.title || '',
+        description: metadata.description || '',
         url: `${process.env.NEXT_PUBLIC_HOST_URL}` + PATH_NAME.home(),
         images: [{ url: metadata.preview_image ?? '' }],
       },
     }
   } else {
-    console.log("Metadata Resp ELSE")
     return {
       title: 'Home | Welcome to Genuin!',
     }
