186e522f44750ddda281021fe283dbc94f26c7e1
Jimit Shah <jimit@begenuin.com>
2024-05-10 17:14:27 +0530

    handled search for normal

--- src/lib/schemas/feed/response.ts
+++ src/lib/schemas/feed/response.ts
@@ -59,7 +59,7 @@ const BrandSchema = z.object({
   favicon: z.string().url(),
   brand_system_user_id: z.string(),
   brand_slug: z.string(),
-})
+}).nullish()
 
 // Define the community schema
 const communitySchema = z
