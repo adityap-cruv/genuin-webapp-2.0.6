a9c59256ffeaa997b42b1db3bf214a06b02f5539
Dev Tejot <devtejot@begenuin.com>
2024-05-27 15:17:16 +0530

    z-index issue fixed

--- src/components/layouts/mobile/top-bar.tsx
+++ src/components/layouts/mobile/top-bar.tsx
@@ -249,11 +249,7 @@ function Menu({
                     community <br /> builder{' '}
                   </span>
                   {embed ? 'for' : 'on'}{' '}
-                  <span
-                    className="truncate text-primary"
-                    style={{
-                      maxWidth: '12ch',
-                    }}>
+                  <span className="inline-block truncate text-primary" style={{ maxWidth: '12ch' }}>
                     {' '}
                     {brandName}
                   </span>{' '}
