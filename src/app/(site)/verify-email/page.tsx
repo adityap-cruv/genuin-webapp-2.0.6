fe74db1645678d31b777a0c4ad891ddbe061138c
Himanshu <himanshu@begenuin.com>
2024-05-27 12:18:46 +0530

    fix: CATEGORY_INPUT screen is being shown after email verification

--- src/app/(site)/verify-email/page.tsx
+++ src/app/(site)/verify-email/page.tsx
@@ -5,11 +5,8 @@ import { analyticsService } from '@services/analytics_service'
 
 export default async function Page({ searchParams }: { searchParams: { token: string } }) {
   const { code, actionMetadata, user, emailType, email } = await verifyEmail(searchParams.token)
-  console.log('code::', code, actionMetadata, user)
   if (code === 200 && actionMetadata?.action === 'KS_CB_REQUEST') {
-    console.log('ks cb req;;')
     const ans = await ksCbRequest(user.accessToken)
-    console.log('ans::', ans)
   }
 
   if (code === 200) {
