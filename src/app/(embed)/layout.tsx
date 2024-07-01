6ae2822ca46982641a101dcbdbfe92b7941e4e48
Jimit Shah <jimit@begenuin.com>
2024-07-01 17:58:43 +0530

    ThirdPartyScriptProvider added

--- src/app/(embed)/layout.tsx
+++ src/app/(embed)/layout.tsx
@@ -8,6 +8,7 @@ import { type ConfigType } from '@lib/stores/genuin-options'
 import { getEmbedConfig } from '@lib/api/config'
 import { parseColors } from '@lib/utils'
 import { BrandNotFound } from '@components/common/brand-not-found'
+import { ThirdPartyScriptProvider } from '@components/providers/third-party-script-provider'
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const configParamsStr = cookies().get('config_params')?.value ?? ''
@@ -29,9 +30,11 @@ export default async function Layout({ children }: { children: ReactNode }) {
   if (error) return <BrandNotFound />
   return (
     <RootHTML brandColors={brandColors} noIndex>
-      <SessionProvider>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </SessionProvider>
+      <ThirdPartyScriptProvider isEmbed={!!config}>
+        <SessionProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </SessionProvider>
+      </ThirdPartyScriptProvider>
     </RootHTML>
   )
 }
