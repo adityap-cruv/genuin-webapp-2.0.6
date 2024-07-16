e4e840da316bf9cfeb8190ecc68f6ad9ac9eab10
Jimit Shah <jimit@begenuin.com>
2024-07-16 14:16:03 +0530

    config issu resolved

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
   if (host) {
     const config = getConfig(host)
     // const config = getConfig('bpatel117.qa.begenuin.com')
-    // request.cookies.set('config_params', JSON.stringify(config))
+    request.cookies.set('config_params', JSON.stringify(config))
     const urlObj = new URL(request.url)
     // eslint-disable-next-line no-prototype-builtins
     if (config && STATIC_PATHNAMES.includes(urlObj.pathname)) {
