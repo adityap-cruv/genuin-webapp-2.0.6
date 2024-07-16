8ebbd3d656241d80704c06769b5953a426e7cab4
Himanshu <himanshu@begenuin.com>
2024-07-16 17:56:12 +0530

    feat: GEN-1468

--- src/components/common/player/control-layer/player-progress-bar.tsx
+++ src/components/common/player/control-layer/player-progress-bar.tsx
@@ -1,14 +1,17 @@
 import { useShallow } from 'zustand/react/shallow'
 import { usePlayerControlStore } from '../player-control-store'
 import { Progress } from '@/components/ui/progress'
+import { useMemo } from 'react'
 
 export function PlayerProgressBar() {
   const { currentTime, duration } = usePlayerControlStore(
     useShallow((state) => ({ currentTime: state.currentTime, duration: state.duration }))
   )
 
-  let progressValue = 0
-  if (duration !== 0) progressValue = Math.round((currentTime / duration) * 100)
+  const progressValue = useMemo(() => {
+    if (duration === 0) return 0
+    return Math.round((currentTime / duration) * 100)
+  }, [currentTime, duration])
 
-  return <Progress value={progressValue} className="absolute bottom-0 left-0 h-[2px] transition-[width]" />
+  return <Progress value={progressValue} className="absolute bottom-0 left-0 h-[2px] transition-all duration-300" />
 }
