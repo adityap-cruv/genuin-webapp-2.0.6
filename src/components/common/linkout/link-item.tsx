bd33e990e892f3d3b52c2e25ab4f23ebdb909634
Jimit Shah <jimit@begenuin.com>
2024-07-18 17:21:23 +0530

    handled links

--- src/components/common/linkout/link-item.tsx
+++ src/components/common/linkout/link-item.tsx
@@ -27,7 +27,7 @@ export function LinkItem({ link, hasImage = false, title, forMobile = false, onC
       <span className="flex items-center gap-2">
         {!title && <LinkIcon className={cn('h-4 w-4 shrink-0', forMobile && 'stroke-monochrome-white')} />}
         <p className={cn('line-clamp-1 break-all text-body-1-demi', forMobile && 'text-monochrome-white')}>
-          {title ?? link}
+          {title || link}
         </p>
       </span>
       {!hasImage && !forMobile && <ChevronRight className="h-4 w-4 shrink-0" />}
