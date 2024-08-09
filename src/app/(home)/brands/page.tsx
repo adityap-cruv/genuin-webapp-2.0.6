260651b84aee6955f4221df59f63ed9170a36e53
Himanshu <himanshu@begenuin.com>
2024-08-09 11:21:39 +0530

    fix: added SEO changes

--- src/app/(home)/brands/page.tsx
+++ src/app/(home)/brands/page.tsx
@@ -1,4 +1,5 @@
 import { BrandsPage } from '@/components/pages/index-pages/brands/brands-page'
+import { type Metadata } from 'next'
 
 export default function Page() {
   return (
@@ -6,4 +7,19 @@ export default function Page() {
       <BrandsPage />
     </main>
   )
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: 'Genuin - Empower Your Brand with Video Communities | For Consumer Brands & Advertisers',
+    description:
+      "Reimagine audience engagement with Genuin's video-based community platform. Create and connect communities across your media channels to drive conversions, unlock first-party data, and expand your brand’s reach. Start your community today and turn consumers into lifelong advocates.",
+    openGraph: {
+      type: 'website',
+      title: 'Genuin - Empower Your Brand with Video Communities | For Consumer Brands & Advertisers',
+      description:
+        "Reimagine audience engagement with Genuin's video-based community platform. Create and connect communities across your media channels to drive conversions, unlock first-party data, and expand your brand’s reach. Start your community today and turn consumers into lifelong advocates.",
+      siteName: 'Genuin',
+    },
+  }
 }
