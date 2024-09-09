b465b8042a1e7b89400418523938998954fce4b8
Dev Tejot <devtejot@begenuin.com>
2024-09-09 12:23:14 +0530

    disabled button and handled error messages for value 0

--- src/components/common/modals/wallet/withdraw-cash.tsx
+++ src/components/common/modals/wallet/withdraw-cash.tsx
@@ -22,6 +22,8 @@ const createSchema = (cashBalance: number) =>
       .refine(
         (value) => {
           const numberValue = Number(value)
+          // Allow '0' or '0.00' without errors; only validate for greater than 0
+          if (numberValue === 0) return true
           return !isNaN(numberValue) && numberValue > 0 && numberValue <= cashBalance / 100
         },
         { message: `Value cannot be greater than $${cashBalance / 100}` }
@@ -39,7 +41,6 @@ export function WithdrawDialog() {
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     mode: 'onBlur',
-    // defaultValues: { amount: 0 },
   })
 
   useEffect(() => {
@@ -118,6 +119,13 @@ export function WithdrawDialog() {
                             const [integerPart, decimalPart] = value.split('.')
                             value = decimalPart.length > 2 ? `${integerPart}.${decimalPart.slice(0, 2)}` : value
                           }
+
+                          // Set no error for zero values
+                          if (Number(value) === 0) {
+                            form.clearErrors('amount')
+                            setErrorMessage('')
+                          }
+
                           field.onChange(value)
                         }}
                       />
@@ -130,7 +138,7 @@ export function WithdrawDialog() {
           />
           <Button
             type="submit"
-            disabled={isLoading || !isValid}
+            disabled={isLoading || !isValid || Number(form.watch('amount')) === 0}
             className="mt-4 flex w-full items-center justify-center border-0">
             {isLoading ? (
               <Loader size="sm" className="fill-new-off-white" />
