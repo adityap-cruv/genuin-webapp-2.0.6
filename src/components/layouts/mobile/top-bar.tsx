b6e8fad727c695746b90e508d3177080fc76e2de
Jimit Shah <jimit@begenuin.com>
2024-09-13 12:38:28 +0530

    handled search icon

--- src/components/layouts/mobile/top-bar.tsx
+++ src/components/layouts/mobile/top-bar.tsx
@@ -132,7 +132,9 @@ export function TopBar({ variant = 'light', className, showClose = false, onClos
               (pathName.includes('/home') ||
                 pathName.includes('/popular') ||
                 pathName.includes('/latest') ||
-                pathName.includes('/video')) &&
+                pathName.includes('/video') ||
+                (pathName.includes('/community') && searchParams.toString().includes('feed=1')) ||
+                (pathName.includes('/loop') && showClose)) &&
               'stroke-monochrome-white'
             }`}
           />
