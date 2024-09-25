8bc203620db81d1f6c8a609f208205b8bb7eafa5
Himanshu <himanshu@begenuin.com>
2024-09-25 17:32:00 +0530

    feat: added on click in video

--- src/components/pages/index-pages/home/index.tsx
+++ src/components/pages/index-pages/home/index.tsx
@@ -126,7 +126,7 @@ export function InitialComponent() {
         </ContactUs>
       }
       // videoSrc="https://media.begenuin.com/backend_assets/hero-video/comp-1_2.m3u8"
-      videoSrc="https://genuin-qa-media.s3.us-west-2.amazonaws.com/genuin-demo-video.mp4"
+      videoSrc="https://genuin-qa-media.s3.us-west-2.amazonaws.com/webapp_assets/temp/final_final.mp4"
     />
   )
 }
