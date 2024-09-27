022bf2fd415ba4ccfa930ca25d412da97cdf850d
Jimit Shah <jimit@begenuin.com>
2024-09-27 14:57:57 +0530

    group path introduced

--- src/lib/utils/constants/path.ts
+++ src/lib/utils/constants/path.ts
@@ -1,6 +1,7 @@
 // TODO: change optional ot non-optional params.
 export const PATH_NAME = {
   loop: (id?: string) => `/loop/${id}`,
+  group: (id?: string) => `/group/${id}`,
   video: (id?: string) => `/video/${id}`,
   profile: (id?: string) => `/profile/${id}`,
   community: (id?: string) => `/community/${id}`,
