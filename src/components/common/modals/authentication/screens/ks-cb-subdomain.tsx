a2e0ba129cdea54cc881ef93f60125da44b8d973
Jimit Shah <jimit@begenuin.com>
2024-10-03 16:31:49 +0530

    Creator corrected

--- src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
+++ src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
@@ -72,7 +72,7 @@ export function KsToCbSubdomain() {
         <SwiperSlide>
           <div className="flex flex-col items-center gap-2">
             <CommunityDiscussion01 className="h-40 fill-primary" />
-            <p className="text-center text-title-1-bold">Become a creator for {brandName}</p>
+            <p className="text-center text-title-1-bold">Become a Creator for {brandName}</p>
             <p className="text-center text-body-1-med ">
               Join us in shaping the future of {brandName} by making your own {brandName} community and expanding it by
               sharing thought-provoking content.
