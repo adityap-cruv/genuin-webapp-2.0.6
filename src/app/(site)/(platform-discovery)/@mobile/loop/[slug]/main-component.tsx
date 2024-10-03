639bb14e6261f80f2454c27909d7a26ae3b2faa8
Jimit Shah <jimit@begenuin.com>
2024-10-03 12:06:24 +0530

    join as member changed

--- src/app/(site)/(platform-discovery)/@mobile/loop/[slug]/main-component.tsx
+++ src/app/(site)/(platform-discovery)/@mobile/loop/[slug]/main-component.tsx
@@ -213,7 +213,7 @@ export function MainComponent({ loopDetails }: Props) {
                   )
                 }}>
                 <p className="px-4 py-1 text-title-3-bold text-primary" style={{ fontSize: '15px' }}>
-                  Join as member
+                  Join as Member
                 </p>
               </Button>
             )}
