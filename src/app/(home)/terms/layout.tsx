62fb837fc99f80c64aaf0fdc74c4d07b85dc8fac
Himanshu Mendapara <himanshu@begenuin.com>
2024-10-11 11:48:33 +0000

    Merged in added-new-navbar (pull request #1085)
    fix: added navbar
    
    * fix: added navbar
    
    
    Approved-by: Krunal Shah

--- src/app/(home)/terms/layout.tsx
+++ src/app/(home)/terms/layout.tsx
@@ -1,5 +1,6 @@
 import { Footer } from '@/components/pages/index-pages/footer'
-import { NavBar } from '@/components/pages/index-pages/nav-bar'
+// import { NavBar } from '@/components/pages/index-pages/nav-bar'
+import { NavBar } from '@/components/new/nav-bar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
