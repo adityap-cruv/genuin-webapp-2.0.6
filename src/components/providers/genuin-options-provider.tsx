ddbe6f05486afb46ed5d5339f5b3b1693a92334a
Jimit Shah <jimit@begenuin.com>
2024-11-07 15:32:19 +0530

    corrected init call

--- src/components/providers/genuin-options-provider.tsx
+++ src/components/providers/genuin-options-provider.tsx
@@ -81,12 +81,6 @@ export function GenuinOptionsProvider({ children, deviceType, os, browserType, c
     }
     void rudderStackIdentify()
 
-    // Have added timeout to track initialized event after 1 sec, because we have to wait for rudderstack to initialize,
-    // Also, We have to keep priorities low for rudderstack to track events.
-    setTimeout(() => {
-      void Analytics.track({ eventName: 'Initialized', properties: {} })
-    }, 1000)
-
     return () => {
       ejectAuthTokenInterceptor(interceptorId)
     }
