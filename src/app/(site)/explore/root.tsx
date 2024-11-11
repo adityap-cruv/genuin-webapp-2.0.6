c18788b020b7f39c4af4a6d29d482ebaf6191f74
Jimit Shah <jimit@begenuin.com>
2024-11-11 17:15:32 +0000

    Merged in feature/GEN-3431/explore-page (pull request #1124)
    Feature/GEN-3431/explore page
    
    * Added Shimmer and handled Loop dynamin loading
    
    * handled loader issue
    
    * handled build error
    
    * handled build issue

--- src/app/(site)/explore/root.tsx
+++ src/app/(site)/explore/root.tsx
@@ -1,17 +1,32 @@
 'use client'
-import { useGenuinOptions } from '@lib/stores/genuin-options'
-import { Loops } from './components'
 import dynamic from 'next/dynamic'
+import { ExploreShimmer } from '@/components/common/shimmers/explore-shimmer'
+import { useGenuinOptions } from '@lib/stores/genuin-options'
 
-const DesktopCommunities = dynamic(async () => await import('./components').then((comp) => comp.Communities.desktop))
-const MobileCommunities = dynamic(async () => await import('./components').then((comp) => comp.Communities.mobile))
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+const DesktopCommunities = dynamic(() => import('./components').then((comp) => comp.Communities.desktop))
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+const MobileCommunities = dynamic(() => import('./components').then((comp) => comp.Communities.mobile))
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+const DynamicLoops = dynamic(() => import('./components').then((comp) => comp.Loops))
 
-export function Root() {
-  const isMobile = useGenuinOptions().isMobile
+export function Root({ isMobile }: { isMobile: boolean }) {
+  const { isLoading } = useGenuinOptions()
+
   return (
     <div className="h-full overflow-auto px-6 pb-6 md:px-4">
-      {isMobile ? <MobileCommunities /> : <DesktopCommunities />}
-      <Loops />
+      {isLoading ? (
+        isMobile ? (
+          <ExploreShimmer.mobile />
+        ) : (
+          <ExploreShimmer.desktop />
+        )
+      ) : (
+        <>
+          {isMobile ? <MobileCommunities /> : <DesktopCommunities />}
+          <DynamicLoops />
+        </>
+      )}
     </div>
   )
 }
