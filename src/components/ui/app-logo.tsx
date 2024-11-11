8bc1fef6a38bbdc3d11de00040f8044bde11d85b
Jimit Shah <jimit@begenuin.com>
2024-11-11 23:25:07 +0530

    handled web logo

--- src/components/ui/app-logo.tsx
+++ src/components/ui/app-logo.tsx
@@ -37,14 +37,14 @@ function LogoProcessor({ className, imageHeight, type, logo, ...props }: Process
 
   if (brandWebLogo)
     return (
-      <div className="relative" style={{ height: imageHeight, width: 150 }}>
-        <img
-          src={brandWebLogo}
-          style={{ height: imageHeight }}
-          className={cn('object-cover', className)}
-          alt="brand logo"
-        />
-      </div>
+      // <div className="relative" style={{ height: imageHeight, width: 150 }}>
+      <img
+        src={brandWebLogo}
+        style={{ height: imageHeight }}
+        className={cn('object-cover', className)}
+        alt="brand logo"
+      />
+      // </div>
     )
 
   switch (type) {
