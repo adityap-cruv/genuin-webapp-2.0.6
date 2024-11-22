34a68099f0d4e588e3679faf3c2da1fd9e1cc489
Krunal Shah <krunal.s@begenuin.com>
2024-11-22 17:10:30 +0530

    Added back skyscape middleware for app association

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,6 +41,22 @@ export async function middleware(request: NextRequest) {
     }
   }
 
+  if (request.nextUrl.pathname.endsWith('/apple-app-site-association') && host) {
+    let config = null
+    try {
+      config = await getEmbedConfig(getConfig(host) ?? {})
+      // TODO: this will be automated by go service just like sitemap
+      // hard coding for buzz skyscape brand
+      if (Number(config?.brand_id) === 2023) {
+        return NextResponse.rewrite(new URL('/.well-known/skyscape/apple-app-site-association', request.url))
+      } else {
+        return NextResponse.next()
+      }
+    } catch (e) {
+      return NextResponse.error()
+    }
+  }
+
   if (request.nextUrl.pathname.endsWith('/assetlinks.json') && host) {
     let config = null
     try {
