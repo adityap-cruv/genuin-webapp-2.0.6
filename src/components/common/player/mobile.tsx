472c07dcff3f7cc1e5d75777718ab6d7702af343
Dev Tejot <134913729+genuindev@users.noreply.github.com>
2024-11-29 10:58:32 +0530

    Added background in the video bottom same as top

--- src/components/common/player/mobile.tsx
+++ src/components/common/player/mobile.tsx
@@ -60,6 +60,7 @@ export function Mobile({
       style={{ backgroundImage: `url(${videoDetails.video.thumbnail})` }}
       className="relative flex h-full w-full snap-start items-center justify-center overflow-clip bg-cover bg-center bg-no-repeat">
       <div className="absolute top-0 z-10 h-24 w-full bg-gradient-to-b from-[#111111b3] to-[#11111100]" />
+      <div className="absolute bottom-0 z-10 h-24 w-full bg-gradient-to-b from-[#11111100] to-[#111111b3]" />
       {!showMore && (
         <div
           className="absolute bottom-0 z-10 h-3/5 w-full bg-gradient-to-b from-[#11111100] to-[#111111] transition-all"
