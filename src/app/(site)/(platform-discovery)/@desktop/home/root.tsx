1ea9bd3218700c055b184535a46b72997b88af74
Dev Tejot <134913729+genuindev@users.noreply.github.com>
2024-12-06 15:06:34 +0530

    EmptyView screens for mobile

--- src/app/(site)/(platform-discovery)/@desktop/home/root.tsx
+++ src/app/(site)/(platform-discovery)/@desktop/home/root.tsx
@@ -3,20 +3,25 @@ import { getFeed } from '@lib/api/feed'
 import { useMemo, useRef } from 'react'
 import { type VideoPlayerModalType } from '@/lib/schemas/player/video'
 import { Feed } from '@/components/common/feed'
+import EmptyView from '@/components/common/empty-view'
 
 export function Root() {
   const { data, fetchNextPage, isLoading, isFetchingNextPage, hasNextPage } = getFeed(1)
   const videosRef = useRef<VideoPlayerModalType[]>([])
   videosRef.current = useMemo(() => data?.pages.flatMap((item) => item.reels) ?? [], [data])
 
-  return (
-    <Feed.desktop
-      hasNextPage={hasNextPage ?? true}
-      isLoading={isLoading}
-      startIndex={0}
-      videosRef={videosRef}
-      isFetchingNextPage={isFetchingNextPage}
-      fetchNextPage={fetchNextPage}
-    />
-  )
+  if (!isLoading && videosRef.current.length === 0) {
+    return <EmptyView type="feed" />
+  } else {
+    return (
+      <Feed.desktop
+        hasNextPage={hasNextPage ?? true}
+        isLoading={isLoading}
+        startIndex={0}
+        videosRef={videosRef}
+        isFetchingNextPage={isFetchingNextPage}
+        fetchNextPage={fetchNextPage}
+      />
+    )
+  }
 }
--- src/app/(site)/(platform-discovery)/@mobile/home/root.tsx
+++ src/app/(site)/(platform-discovery)/@mobile/home/root.tsx
@@ -2,26 +2,31 @@
 import { getFeed } from '@lib/api/feed'
 import { Feed } from '@/components/common/feed'
 import dynamic from 'next/dynamic'
+import EmptyView from '@/components/common/empty-view'
 const TopBar = dynamic(async () => await import('@components/layouts/mobile/top-bar').then((comp) => comp.TopBar))
 
 export function Root() {
   const { data: videoPages, isError, isLoading, isFetchingNextPage, hasNextPage, fetchNextPage } = getFeed(1)
   const videos = videoPages?.pages.flatMap((item) => item.reels)
 
-  return (
-    <main className="h-full w-full">
-      <TopBar variant="transparent" />
-      <span className="absolute inset-0">
-        <Feed.mobile
-          startIndex={0}
-          fetchNextPage={fetchNextPage}
-          isError={isError}
-          isFetchingNextPage={isFetchingNextPage}
-          hasNextPage={hasNextPage}
-          isLoading={isLoading}
-          videos={videos}
-        />
-      </span>
-    </main>
-  )
+  if (!isLoading && videos?.length === 0) {
+    return <EmptyView type="feed" />
+  } else {
+    return (
+      <main className="h-full w-full">
+        <TopBar variant="transparent" />
+        <span className="absolute inset-0">
+          <Feed.mobile
+            startIndex={0}
+            fetchNextPage={fetchNextPage}
+            isError={isError}
+            isFetchingNextPage={isFetchingNextPage}
+            hasNextPage={hasNextPage}
+            isLoading={isLoading}
+            videos={videos}
+          />
+        </span>
+      </main>
+    )
+  }
 }
