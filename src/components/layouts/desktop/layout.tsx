a42710322343f0aaa5194de942eb883fdcb1a437
Himanshu Mendapara <himanshu@begenuin.com>
2024-12-11 16:12:47 +0000

    Merged in feature/iheart-demo (pull request #1166)
    IHeart demo configured.
    
    * basic layout added
    
    * dynamic handling added
    
    * commented unused line
    
    * feat: IHeart demo support addd
    
    * feat: iheart demo
    
    
    Approved-by: Jimit Shah

--- src/components/layouts/desktop/layout.tsx
+++ src/components/layouts/desktop/layout.tsx
@@ -3,9 +3,13 @@ import { SideBar } from './side-bar'
 import { TopBar } from './top-bar'
 import { cn } from '@lib/utils'
 import { useGenuinOptions } from '@lib/stores/genuin-options'
+import { IHeartDemo } from './iheart-demo'
+import { useIHeartDemoStates } from '@/components/providers/iheart-demo-provider'
 
 export function Layout(props: any) {
-  const showNavbar = useGenuinOptions().showNavbar
+  const { showNavbar } = useGenuinOptions()
+  const { renderIn } = useIHeartDemoStates()
+
   return (
     <main className="absolute inset-0 flex h-full w-full flex-col items-center overflow-clip">
       {showNavbar && <TopBar />}
@@ -15,8 +19,12 @@ export function Layout(props: any) {
           showNavbar ? 'h-body' : 'h-full'
         )}>
         <SideBar />
-        <section className="relative w-full">{props.children}</section>
+        <section id="root-element" className="relative flex w-full flex-col">
+          {props.children}
+        </section>
       </section>
+      {/* Iframe Section */}
+      {renderIn === 'root' && <IHeartDemo />}
     </main>
   )
 }
