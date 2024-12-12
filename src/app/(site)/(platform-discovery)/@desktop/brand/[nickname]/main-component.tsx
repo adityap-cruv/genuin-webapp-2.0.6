8e922d5e0170252d08c8a4aeb10e30b43b9f6286
Jimit Shah <jimit@begenuin.com>
2024-12-12 13:26:14 +0530

    icon added

--- src/app/(site)/(platform-discovery)/@desktop/brand/[nickname]/main-component.tsx
+++ src/app/(site)/(platform-discovery)/@desktop/brand/[nickname]/main-component.tsx
@@ -18,6 +18,8 @@ import { TikTokIcon } from '@icons/tiktok-icon'
 import { LinkedInIcon } from '@icons/linkedin-icon'
 import { TwitterIcon } from '@icons/twitter-icon'
 import Analytics from '@services/analytics'
+import { useGenuinOptions } from '@/lib/stores/genuin-options'
+import { LinkIcon } from '@icons/link-icon'
 
 interface CompProps {
   profileData: ProfileDetailsType
@@ -100,9 +102,17 @@ function Links({ profileData }: CompProps) {
   }
   const { shareFn } = useAdaptiveShare()
   const { toast } = useToast()
+  const brandId = useGenuinOptions().brandId
 
   return (
     <div className="my-2 flex items-center">
+      {brandId && profileData.brand?.brand_id !== Number(brandId) && (
+        <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-md bg-tertiary-200 p-1">
+          <Link href={checkAndAppendHttps(profileData.brand?.brand_url ?? '')} target="_blank">
+            <LinkIcon className="h-5 w-5 stroke-primary " />
+          </Link>
+        </div>
+      )}
       {links?.linkedin && (
         <div className="mr-2 flex h-8 w-8 items-center justify-center rounded-md bg-tertiary-200 p-1">
           <Link href={checkAndAppendHttps(links.linkedin)} target="_blank">
