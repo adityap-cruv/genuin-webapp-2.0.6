b414423834e342286ffbd9055484f30825a63f2a
Jimit Shah <jimit@begenuin.com>
2024-12-16 21:42:23 +0530

    handled claim brand

--- src/components/layouts/mobile/top-bar.tsx
+++ src/components/layouts/mobile/top-bar.tsx
@@ -232,7 +232,7 @@ function Menu({
             <p className={cn('whitespace-nowrap !text-title-3-demi text-primary')}>Log in</p>
           </div>
         )}
-        {!isClaimed && (
+        {!isClaimed && embed && (
           <div
             className="flex w-full max-w-full shrink-0 items-center gap-x-3 rounded-md p-2 px-4 hover:bg-monochrome-6/10"
             onClick={() => {
