a7b5b28f2b433d8952fa0b171b0a222a89672adf
Krunal Shah <krunal.s@begenuin.com>
2024-12-20 15:10:30 +0530

    Added gap for download app on desktop

--- src/components/layouts/desktop/top-bar.tsx
+++ src/components/layouts/desktop/top-bar.tsx
@@ -60,7 +60,7 @@ export function TopBar({
                 />
               </div>
             )}
-            <div className="flex items-center gap-x-2">
+            <div className="flex items-center gap-x-3">
               {showSearchBar && <SearchBar.desktop />}
               <WalletAmountBadge type="dark" />
               {!isEmbed ? (
