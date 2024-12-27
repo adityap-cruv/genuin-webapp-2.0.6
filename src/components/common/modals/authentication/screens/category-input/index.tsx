18c25c7965ba33691b1427b44a9619b2819ccaa9
Jimit Shah <jimit@begenuin.com>
2024-12-27 16:27:02 +0530

    fixed bug

--- src/components/common/modals/authentication/screens/category-input/index.tsx
+++ src/components/common/modals/authentication/screens/category-input/index.tsx
@@ -50,76 +50,81 @@ export function CategoryInput({ onNext }: ScreenProps) {
 
   return (
     <ModalShell className="max-h-[60vh] pb-0">
-      <span className="text-center">
-        <p className="text-heading-3">What are you interested in?</p>
-        <p className="text-title-3-med text-tertiary">
-          Get started by picking three topics you're interested in, to see more of what you love.
-        </p>
-      </span>
-      {isLoading ? (
-        <div className="flex h-[40vh] w-full items-center justify-center">
+      {postingTopics ? (
+        <div className="flex h-[50vh] w-full flex-col items-center justify-center text-center align-middle">
           <Loader size="md" />
+          <p>Customizing your feed...</p>
         </div>
       ) : (
-        <div className="h-full w-full overflow-x-clip overflow-y-scroll">
-          {data?.map((item) => {
-            return (
-              <div className="border-b border-b-tertiary-200 py-4 first:pt-0" key={item.entity_id}>
-                <p className="pb-2 text-title-3-demi">{item.title}</p>
-                <div className="flex w-full max-w-full flex-wrap overflow-clip">
-                  {item.topics.map((item) => {
-                    return (
+        <>
+          <span className="text-center">
+            <p className="text-heading-3">What are you interested in?</p>
+            <p className="text-title-3-med text-tertiary">
+              Get started by picking three topics you're interested in, to see more of what you love.
+            </p>
+          </span>
+          {isLoading ? (
+            <div className="flex h-[40vh] w-full items-center justify-center">
+              <Loader size="md" />
+            </div>
+          ) : (
+            <div className="h-full w-full overflow-x-clip overflow-y-scroll">
+              {data?.map((item) => (
+                <div className="border-b border-b-tertiary-200 py-4 first:pt-0" key={item.entity_id}>
+                  <p className="pb-2 text-title-3-demi">{item.title}</p>
+                  <div className="flex w-full max-w-full flex-wrap overflow-clip">
+                    {item.topics.map((topic) => (
                       <p
-                        onClick={(e) => {
+                        onClick={() =>
                           setSelectedItem((state) => {
                             const newState = new Set([...state.values()])
-                            if (newState.has(item.topic_id)) {
-                              newState.delete(item.topic_id)
+                            if (newState.has(topic.topic_id)) {
+                              newState.delete(topic.topic_id)
                             } else {
-                              newState.add(item.topic_id)
+                              newState.add(topic.topic_id)
                             }
                             return newState
                           })
-                        }}
+                        }
                         className={cn(
                           'my-1 mr-2 line-clamp-1 w-fit cursor-pointer overflow-hidden break-all rounded-full border border-tertiary-200 px-3 py-1 text-body-1-demi leading-loose',
-                          selectedItems.has(item.topic_id) && 'border-primary bg-primary-100'
+                          selectedItems.has(topic.topic_id) && 'border-primary bg-primary-100'
                         )}
-                        key={item.topic_id}>
-                        {item.topic}
+                        key={topic.topic_id}>
+                        {topic.topic}
                       </p>
-                    )
-                  })}
+                    ))}
+                  </div>
                 </div>
-              </div>
-            )
-          })}
-        </div>
+              ))}
+            </div>
+          )}
+          <Button
+            onClick={async () => {
+              await handleSubmit()
+            }}
+            className="w-full"
+            disabled={selectedItems.size < 3}>
+            {postingTopics ? (
+              <Loader size="md" className="fill-monochrome-white" />
+            ) : (
+              <p className="text-title-3-demi">{selectedItems.size < 3 ? 'Choose 3+' : 'Continue'}</p>
+            )}
+          </Button>
+          <Button
+            onClick={async () => {
+              await handleSubmit('all')
+            }}
+            variant="outline"
+            className="h-9 w-full">
+            {postingTopics ? (
+              <Loader size="sm" className="fill-monochrome-white" />
+            ) : (
+              <p className="text-title-3-demi">Surprise Me</p>
+            )}
+          </Button>
+        </>
       )}
-      <Button
-        onClick={async () => {
-          await handleSubmit()
-        }}
-        className="w-full"
-        disabled={selectedItems.size < 3}>
-        {postingTopics ? (
-          <Loader size="md" className="fill-monochrome-white" />
-        ) : (
-          <p className="text-title-3-demi">{selectedItems.size < 3 ? 'Choose 3+' : 'Continue'}</p>
-        )}
-      </Button>
-      <Button
-        onClick={async () => {
-          await handleSubmit('all')
-        }}
-        variant={'outline'}
-        className="h-9 w-full">
-        {postingTopics ? (
-          <Loader size="sm" className="fill-monochrome-white" />
-        ) : (
-          <p className="text-title-3-demi">Surprise Me</p>
-        )}
-      </Button>
       {error && (
         <p className="text-text-new-para-2-mobile flex items-center justify-center text-center text-supplementary-red">
           {error}
