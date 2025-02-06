de991b1216d8ea7d56df10f354ad7f98d50b2eb0
Dev Tejot <134913729+genuindev@users.noreply.github.com>
2025-02-06 11:45:15 +0530

    description height changes

--- src/components/common/read-more.tsx
+++ src/components/common/read-more.tsx
@@ -392,7 +392,7 @@ export function Dynamic({
     <p
       {...props}
       className={cn(
-        'whitespace-pre-wrap transition-[max-height] duration-500 sm:max-h-full',
+        'whitespace-pre-wrap transition-[max-height] duration-500 sm:max-h-max',
         {
           'swiper-no-swiping hide-scrollbar': isExpanded && isMobile,
         },
