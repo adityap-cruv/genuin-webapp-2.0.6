ee6d79b88aab61e39d8427ef401bf9b9e8f2be12
krunals3 <krunal.s@begenuin.com>
2025-02-19 18:09:24 +0530

    Remove QA environment-specific deep link replacement logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -240,9 +240,6 @@ export const generateDeepLink = async ({
       finalPayload
     )
     const shortLink = res?.data?.data?.shortLink
-    if (shortLink?.includes('coldplay.qa.begenuin.com')) {
-      return shortLink.replace('https://coldplay.qa.begenuin.com', 'http://192.168.0.198:4005')
-    }
     return shortLink
   } catch (e) {
     return process.env.NEXT_PUBLIC_HOST_URL
