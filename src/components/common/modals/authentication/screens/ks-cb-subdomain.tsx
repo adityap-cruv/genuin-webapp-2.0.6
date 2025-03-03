3050a933d5b75ba13c6b122fd047c1f6020e10f2
Himanshu Mendapara <himanshu@begenuin.com>
2025-03-03 18:31:01 +0530

    fix: ks cb modal fixed

--- src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
+++ src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
@@ -83,9 +83,9 @@ export function KsToCbSubdomain() {
         <SwiperSlide>
           <div className="flex flex-col items-center gap-2">
             <CommunityDiscussion02 className="h-40 fill-primary" />
-            <p className="text-center text-title-1-bold">Make Connections & Spark Dialogues</p>
+            <p className="text-center text-title-1-bold">Make Connections & React on Dialogues</p>
             <p className="text-center text-body-1-med">
-              Invite others to join your {brandName} community, share engaging content, and spark meaningful
+              Invite others to join your {brandName} community, share engaging content, and react on meaningful
               conversations to make connections and foster intellectual dialogue.
             </p>
             <br />
