0e29dddb06e80141557407f09aa806952f3b980a
Dev Tejot <134913729+genuindev@users.noreply.github.com>
2025-03-03 14:46:51 +0530

    home-zone default embed added

--- src/hooks/use-embed-details.tsx
+++ src/hooks/use-embed-details.tsx
@@ -19,39 +19,39 @@ export function useEmbedSetup({ config }: { config: ConfigType | undefined }) {
 
   const defaultEmbedConfigs = {
     'Home/Search Embed': {
-      embedId: '67c17f9fe0ac202848d7ac5c',
+      embedId: '67c55cdac71e679e40b2497c',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'Blog Embed': {
-      embedId: '67c17f9fe0ac202848d7ac5d',
+      embedId: '67c55cdac71e679e40b2497d',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'Home/Search Embed1': {
-      embedId: '67c17f9fe0ac202848d7ac5e',
+      embedId: '67c55cdac71e679e40b2497e',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'Post-Sales Embed': {
-      embedId: '67c17f9fe0ac202848d7ac5f',
+      embedId: '67c55cdac71e679e40b2497f',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'PDP Embed': {
-      embedId: '67c17f9fe0ac202848d7ac60',
+      embedId: '67c55cdac71e679e40b24980',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'Home/Blog Embed': {
-      embedId: '67c17f9fe0ac202848d7ac61',
+      embedId: '67c55cdac71e679e40b24981',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
     'Home/Post Sales Embed': {
-      embedId: '67c17f9fe0ac202848d7ac62',
+      embedId: '67c55cdac71e679e40b24982',
       embedType: 'brand_feed',
-      embedApiKey: 'e895c22b1281e14ff16405aa54f1d68ac6238de9b709383c',
+      embedApiKey: '0bb6e6e8667a66183edeafa919f4f1b3e725a92021fa5b87',
     },
   }
 
