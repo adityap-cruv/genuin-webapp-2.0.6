b73b92db1511396529882ad290d57752155be4ec
Himanshu Mendapara <himanshu@begenuin.com>
2025-03-04 16:11:48 +0530

    fix: fallback removed

--- src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
+++ src/components/common/modals/authentication/screens/ks-cb-subdomain.tsx
@@ -100,7 +100,8 @@ export function KsToCbSubdomain() {
             <CommunityDiscussion03 className="h-40 fill-primary" />
             <p className="text-center text-title-1-bold">Moderate your Community</p>
             <p className="text-center text-body-1-med">
-              Create a safe space where your members can thrive. Customize your community with guidelines, add and more.
+              Create a safe space where your members can thrive. Customize your community with guidelines, add admins,
+              and more.
             </p>
             <br />
           </div>
