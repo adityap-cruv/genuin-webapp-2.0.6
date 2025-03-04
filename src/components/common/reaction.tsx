912ebeae97330509583a6130c693a195d6a4fd2a
Dev Tejot <devtejot@ip-192-168-0-179.us-west-2.compute.internal>
2025-03-04 19:57:20 +0530

    build fix

--- src/components/common/reaction.tsx
+++ src/components/common/reaction.tsx
@@ -57,11 +57,11 @@ export function Reaction({
         }
       } else {
         // This call will generate the deep link for the video.
-        await sparkDeepLink(videoSlug, shareUrl, config.reactions.suffix, config.reactions.title).then(
+        await sparkDeepLink(videoSlug, shareUrl, config?.reactions.suffix ?? '', config?.reactions.title ?? '').then(
           (generatedLink) => {
             openModal({
               deepLink: generatedLink,
-              subtitle: `Get the app to ${config.reactions.title + ' ' + config.reactions.suffix} the video.`,
+              subtitle: `Get the app to ${config?.reactions?.title + ' ' + config?.reactions?.suffix} the video.`,
             })
           }
         )
