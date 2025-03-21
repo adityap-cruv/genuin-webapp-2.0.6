19d84dd5555822707e43d27e1f701215fef2ad80
Dev Tejot <devtejot@begenuin.com>
2025-03-21 12:54:22 +0000

    Merged in hotfix/GEN-5437/explore_scroll (pull request #1307)
    GEN-5437: scroll in mobile explore page
    
    * GEN-5437: scroll in mobile explore page
    
    
    Approved-by: Krunal Shah

--- src/app/(site)/explore/page.tsx
+++ src/app/(site)/explore/page.tsx
@@ -3,7 +3,7 @@ import { Loops } from '@/components/pages/explore/loops'
 
 export default function Page() {
   return (
-    <div className="h-full overflow-auto px-6 pb-6 md:px-4">
+    <div className="overflow-auto px-4 pb-6">
       <Communities />
       <Loops />
     </div>
