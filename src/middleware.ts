cba4f7a773d5408efdb4787322c5887e611d3112
Jimit Shah <jimit@begenuin.com>
2025-04-07 10:48:06 +0000

    Merged in carlist-handle (pull request #1362)
    GEN-5691 | Assetlink.json middleware handle
    
    * GEN-5691 | Assetlink.json middleware handle
    
    
    Approved-by: Himanshu Mendapara

--- src/middleware.ts
+++ src/middleware.ts
@@ -98,6 +98,9 @@ export async function middleware(request: NextRequest) {
       } else if (Number(config?.brand_id) === 1429) {
         // hard coding for ankpal in QA
         return NextResponse.rewrite(new URL('/.well-known/ankpal/assetlinks.json', request.url))
+      } else if (Number(config?.brand_id) === 2314) {
+        // hard coding for carlist
+        return NextResponse.rewrite(new URL('/.well-known/carlist/assetlinks.json', request.url))
       } else if (Number(config?.brand_id) === 2357) {
         // hard coding for ted
         return NextResponse.rewrite(new URL('/.well-known/ted/assetlinks.json', request.url))
