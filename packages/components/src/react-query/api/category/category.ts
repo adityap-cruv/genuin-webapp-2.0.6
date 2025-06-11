9cf2caca219b9855b53b1629892d80a414fe563c
krunals3 <krunal.s@begenuin.com>
2025-06-11 16:37:56 +0530

    GEN-5680: Fix components package react-query resolution

--- packages/components/src/react-query/api/category/category.ts
+++ packages/components/src/react-query/api/category/category.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "@react-query/axios-instance";
 import { useQuery } from "@tanstack/react-query";
-import { getQueryKeyForCategories } from "@react-query/keys/category";
+import { getQueryKeyForCategories } from "@react-query/keys/authentication";
 import { parseCategory } from "./index";
 
 async function fetchCategories() {
