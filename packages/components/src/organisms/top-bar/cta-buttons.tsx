b1e56fd481626e37e78f88f355ffae4dd136b4d0
Himanshu Mendapara <himanshu@begenuin.com>
2025-06-13 11:26:37 +0000

    Merged in feature/user-tick (pull request #1523)
    feat(GEN-5946): added user-tick
    
    * feat(GEN-5946): added user-tick
    
    
    Approved-by: Krunal Shah

--- packages/components/src/organisms/top-bar/cta-buttons.tsx
+++ packages/components/src/organisms/top-bar/cta-buttons.tsx
@@ -1,29 +1,126 @@
 import { Button } from "@genuin/ui/button";
 import { useBaseContext } from "@genuin/components/context/base";
+import { AuthenticationModal } from "@organisms/authentication-modal";
+import { useAuthContext } from "@context/auth";
+import { Popover, PopoverContent, PopoverTrigger } from "@genuin/ui/popover";
+import { Avatar } from "@genuin/ui/avatar";
+import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { Link } from "@molecules/link";
+import { buildPageUrl } from "@lib/utils/pages";
 
 export function CtaButtons() {
   const { web_cta } = useBaseContext().brandDetails;
+  const { authenticationStatus } = useAuthContext();
+
+  const showApp = web_cta === "app" || web_cta === "both";
+  const showLogin =
+    (web_cta === "login" || web_cta === "both") &&
+    authenticationStatus === "unauthenticated";
+
+  const showUserTick = authenticationStatus === "authenticated";
   return (
-    // <div className="gencl:flex gencl:justify-between gencl:gap-2.5">
-    //   <div className="gencl:w-72 gencl:h-2"></div>
     <div className="gencl:flex gencl:gap-2.5 gencl:justify-between gencl:items-center">
-      {(web_cta === "app" || web_cta == "both") && (
-        <Button
-          theme="outline"
-          className="gencl:min-w-[80px] gencl:h-9 gencl:px-4 gencl:py-1.5 gencl:flex-shrink-0 gencl:text-body-1-semi-bold"
-        >
-          Get app
-        </Button>
+      {true && (
+        <AuthenticationModal asChild>
+          <Button theme="outline" size="sm">
+            Get app
+          </Button>
+        </AuthenticationModal>
       )}
-      {(web_cta === "login" || web_cta == "both") && (
-        <Button
-          theme="primary"
-          className="gencl:min-w-[80px] gencl:h-9 gencl:px-4 gencl:py-1.5 gencl:flex-shrink-0 gencl:text-body-1-bold"
-        >
-          Log in
-        </Button>
+      {showLogin && (
+        <AuthenticationModal asChild>
+          <Button theme="primary" size="sm">
+            Log in
+          </Button>
+        </AuthenticationModal>
       )}
+      {showUserTick && <UserTick />}
     </div>
-    // </div>
+  );
+}
+
+function UserTick() {
+  const { user } = useAuthContext();
+  if (!user) {
+    return null; // or handle the case where user is not defined
+  }
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Avatar
+          imageUrl={user.image}
+          isAvatar={user.isAvatar}
+          alt={user.name}
+        />
+      </PopoverTrigger>
+      <PopoverContent
+        avoidCollisions
+        sideOffset={8}
+        collisionPadding={{ right: 16 }}
+      >
+        <Content />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function Content() {
+  const { user, signOut } = useAuthContext();
+
+  const ACTIONS = [
+    {
+      icon: <LogOutIcon className="gencl:size-6" />,
+      label: "Log out",
+      onClick: () => signOut("/"),
+    },
+  ] as const;
+
+  if (!user) {
+    return null; // or handle the case where user is not defined
+  }
+
+  // TODO: Have clarity from desgn on which button type to use here.
+  // TODO: add a component for the settings link.
+  return (
+    <div className="gencl:flex gencl:flex-col gencl:gap-2">
+      <div className="gencl:border-b gencl:border-secondary-150 gencl:pb-3">
+        <div className="gencl:flex gencl:items-center gencl:gap-2">
+          <Avatar
+            imageUrl={user.image}
+            isAvatar={user.isAvatar}
+            alt={user.name}
+            size="md"
+          />
+          <div className="">
+            <p className="gencl:text-body-1-bold">
+              {user.name ?? "@" + user.nickname}
+            </p>
+            {!user.isBrandSystemUser && (
+              // <AuthenticationModal customStep="COMPLETE_PROFILE">
+              <Link href={buildPageUrl({ type: "settings" })}>
+                <p className="gencl:text-body-1-semi-bold gencl:cursor-pointer gencl:text-primary gencl:hover:text-primary-700">
+                  Complete Profile
+                </p>
+              </Link>
+              // </AuthenticationModal>
+            )}
+          </div>
+        </div>
+      </div>
+      <Link href={buildPageUrl({ type: "settings" })}>
+        <div className="gencl:flex gencl:items-center gencl:gap-2 gencl:px-2 gencl:hover:bg-secondary-50 gencl:py-3 gencl:rounded-lg gencl:text-body-1-medium">
+          <SettingsIcon />
+          Settings
+        </div>
+      </Link>
+      <Button
+        className="gencl:px-2 gencl:hover:bg-secondary-50 gencl:text-body-1-medium! gencl:justify-start"
+        theme="custom"
+        onClick={() => signOut(buildPageUrl({ type: "home" }))}
+      >
+        <LogOutIcon className="gencl:size-6" />
+        Log out
+      </Button>
+    </div>
   );
 }
--- packages/ui/src/components/popover/popover.tsx
+++ packages/ui/src/components/popover/popover.tsx
@@ -30,7 +30,21 @@ function PopoverContent({
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden",
+          [
+            // Base styles
+            "gencl:z-50 gencl:w-60 gencl:bg-white gencl:rounded-md gencl:p-4 gencl:shadow-mdc gencl:outline-hidden",
+            // Transform origin
+            "gencl:origin-(--radix-popover-content-transform-origin) gencl:border-secondary-150 gencl:border",
+            // Animation states
+            "data-[state=open]:gencl:animate-in data-[state=closed]:gencl:animate-out",
+            "data-[state=open]:gencl:fade-in-0 data-[state=closed]:gencl:fade-out-0",
+            "data-[state=open]:gencl:zoom-in-95 data-[state=closed]:gencl:zoom-out-95",
+            // Side-specific slide animations
+            "data-[side=bottom]:gencl:slide-in-from-top-2",
+            "data-[side=left]:gencl:slide-in-from-right-2",
+            "data-[side=right]:gencl:slide-in-from-left-2",
+            "data-[side=top]:gencl:slide-in-from-bottom-2",
+          ],
           className
         )}
         {...props}
