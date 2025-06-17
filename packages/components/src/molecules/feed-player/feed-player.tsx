e49fd423313182b4ea101b14ec8c8ee24ef90b31
Dhairya Patel <dhairya.p@begenuin.com>
2025-06-17 12:28:19 +0000

    Merged in bugfix/shimmers (pull request #1559)
    fix(GEN-6024 : remove dummy comment)
    
    * fix(GEN-6024 : remove dummy comment)
    
    
    Approved-by: Himanshu Mendapara

--- packages/components/src/molecules/feed-player/feed-player.tsx
+++ packages/components/src/molecules/feed-player/feed-player.tsx
@@ -1,13 +1,11 @@
 import { VideoPlayer } from "@genuin/ui/video-player";
-import { memo, useCallback, type ComponentProps } from "react";
+import { memo, useCallback, useEffect, useId, type ComponentProps } from "react";
 import { useBaseContext } from "@genuin/components/context/base";
 import type { PostDetailsType } from "@genuin/components/react-query/api/feed/schema";
-
+import { audioManager } from "@genuin/components/lib/audio-manager";
 import { usePlayerContext } from "./context/context";
 import { useFeedContext } from "@genuin/components/templates/feed/context";
-// import { useSwiper } from "swiper/react";
-
-// import { usePlayerContext } from "./context";
+import { useAnalytics } from "@genuin/components/context/analytics";
 
 type Props = Omit<
   ComponentProps<typeof VideoPlayer>,
@@ -39,214 +37,27 @@ export const FeedPlayer = memo(function FeedPlayer({
     unmute,
   } = usePlayerContext();
   const { playbackSpeed } = useFeedContext();
+  const Analytics = useAnalytics()
+    const id = useId();
 
-  // const { brandId } = useGenuinOptions(
-  //   useShallow((state) => ({
-  //     brandId: state.brandId,
-  //   }))
-  // );
-  // const {
-  //   playerRef,
-  //   videoRef,
-  //   setTimeState: singleVideoSetTimeState,
-  //   playerConfigRef,
-  //   resetPlayerConfig,
-  //   play: contextPlay,
-  // } = usePlayerContext();
-  // const {
-  //   shouldPlay,
-  //   muted,
-  //   setTimeState,
-  //   volume,
-  //   setPlayingState,
-  //   setShouldPlay,
-  //   playbackSpeed,
-  // } = usePlayerControlStore(
-  //   useShallow((state) => ({
-  //     shouldPlay: state.shouldPlay,
-  //     muted: state.muted,
-  //     setTimeState: state.setTimeState,
-  //     volume: state.volume,
-  //     setPlayingState: state.setPlayingState,
-  //     toggleMuted: state.toggleMuted,
-  //     setShouldPlay: state.setShouldPlay,
-  //     playbackSpeed: state.playbackSpeed,
-  //   }))
-  // );
-  // const { showGestureOverlay } = useGestureOverlayManager();
-  // const { currentIndex } = useFeedListContext();
-  // const swiper = useSwiper();
-  // const webConfigs = useGenuinOptions((state) => state.config.web_configs);
-
-  // let encodedVideoSourceUrl = videoSource;
-  // if (brandId && brandId.toString() === "1729") {
-  //   // Replace the video source URL with the OCITest URL
-  //   encodedVideoSourceUrl = encodedVideoSourceUrl.replace(
-  //     "media.begenuin.com",
-  //     "ocitest.begenuin.com"
-  //   );
-  // }
-  // // Create a URL object to easily access query parameters
-  // const videoUrl = new URL(videoSource);
-  // // If there are no query parameters meaning either it's m3u8 without query params or mp4 file
-  // if (videoUrl.search) {
-  //   const { appendParamsToUrl } = useUrlParams();
-  //   const macrosUpdatedVideoSource = appendParamsToUrl(videoSource);
-  //   encodedVideoSourceUrl = encodeVideoSourceUrl(macrosUpdatedVideoSource);
-  // }
-
-  // const initializePlayer = useCallback(
-  //   async (player: OpenPlayerJS, playAfterInit: boolean) => {
-  //     await player.init();
-  //     await player.load();
-  //     // setPlayingState(undefined);
-  //     if (playAfterInit) {
-  //       await contextPlay(player).catch(async (error) => {
-  //         console.log("error in player", error);
-  //       });
-  //     }
-  //     return player;
-  //   },
-  //   [contextPlay, setPlayingState]
-  // );
-
-  // useEffect(() => {
-  //   // Reset hasStarted when video source changes
-  //   // resetPlayerConfig(webConfigs);
-
-  //   if (!videoRef.current) return;
-  //   const player = new OpenPlayerJS(videoRef.current, {
-  //     controls: {
-  //       alwaysVisible: false,
-  //     },
-  //     mode: "responsive",
-  //     forceNative: true,
-  //     showLoaderOnInit: true,
-  //     onError: (e) => {
-  //       console.error(e, "error");
-  //     },
-  //     hls: hlsConfigs,
-  //   });
-
-  //   // Set initial playback speed for the new video
-  //   videoRef.current.playbackRate = playbackSpeed.speed;
-
-  //   void initializePlayer(
-  //     player,
-  //     isActive && playerConfigRef.current.autoplay
-  //   ).then((initializedPlayer) => {
-  //     playerRef.current = initializedPlayer;
-
-  //     // Ensure playback speed is set after initialization
-  //     if (videoRef.current) {
-  //       videoRef.current.playbackRate = playbackSpeed.speed;
-  //     }
-  //   });
-
-  //   if (isActive && !playerConfigRef.current.autoplay) {
-  //     setShouldPlay(false);
-  //   }
-  // }, [videoSource]);
-
-  // useEffect(() => {
-  //   const player = playerRef.current;
-  //   if (!player) return;
-
-  //   if (isActive) {
-  //     if (shouldPlay) {
-  //       void contextPlay(player);
-  //     } else {
-  //       player.pause();
-  //     }
-  //   } else {
-  //     player.pause();
-  //   }
-  // }, [isActive, shouldPlay, contextPlay]);
-
-  // useEffect(() => {
-  //   if (isActive && !playerConfigRef.current.autoplay) {
-  //     setShouldPlay(false);
-  //   }
-  // }, [isActive]);
-
-  // const onTimeUpdateEventHandler: ReactEventHandler<HTMLVideoElement> =
-  //   useCallback(
-  //     (event: React.SyntheticEvent<HTMLVideoElement>) => {
-  //       const video = event.currentTarget;
-
-  //       // setTimeState(video.currentTime, video.duration, id);
-  //       singleVideoSetTimeState(video.currentTime, video.duration);
-
-  //       if (video.duration > 0) {
-  //         const progress = (video.currentTime / video.duration) * 100;
-  //         // if (currentIndex === 1 && progress >= 50) {
-  //         //   showGestureOverlay("PLAY_PAUSE", muted);
-  //         // }
-  //       }
-  //     },
-  //     []
-  //   );
-
-  // const handlePlaying: ReactEventHandler<HTMLVideoElement> = useCallback(
-  //   (ev) => {
-  //     onPlaying?.(ev);
-  //     setPlayingState("playing");
-  //   },
-  //   [onPlaying, setPlayingState]
-  // );
-
-  // const handleError: ReactEventHandler<HTMLVideoElement> = useCallback(
-  //   (ev) => {
-  //     onError?.(ev);
-  //     setPlayingState("paused");
-  //   },
-  //   [onError, setPlayingState]
-  // );
-
-  // const handlePause: ReactEventHandler<HTMLVideoElement> = useCallback(
-  //   (ev) => {
-  //     onPause?.(ev);
-  //     setPlayingState("paused");
-  //     triggerAnalyticsForVideoPause(id);
-  //   },
-  //   [onPause, setPlayingState, id]
-  // );
-
-  // const handleEnded: ReactEventHandler<HTMLVideoElement> = useCallback(
-  //   (ev) => {
-  //     onEnded?.(ev);
-  //     const { repeatCount, shouldSwipeNext } = playerConfigRef.current;
-  //     if (repeatCount > 0 && playerRef.current) {
-  //       playerConfigRef.current.repeatCount--;
-  //       void playerRef.current.play();
-  //       return;
-  //     } else {
-  //       resetPlayerConfig(webConfigs);
-  //       setShouldPlay(false);
-  //     }
-
-  //     if (shouldSwipeNext && swiper) {
-  //       resetPlayerConfig(webConfigs);
-  //       swiper.slideNext();
-  //     }
-  //   },
-  //   [
-  //     onEnded,
-  //     playerConfigRef,
-  //     resetPlayerConfig,
-  //     playerRef,
-  //     setShouldPlay,
-  //     swiper,
-  //     webConfigs,
-  //   ]
-  // );
-
-  // const handleLoadStart: ReactEventHandler<HTMLVideoElement> = useCallback(
-  //   (ev) => {
-  //     onLoadStart?.(ev);
-  //   },
-  //   [onLoadStart]
-  // );
+    useEffect(() => {
+    audioManager.register(id, () => {
+      mute(true);
+    });
+    return () => {
+      audioManager.unregister(id);
+    };
+  }, [id]);
+  
+  useEffect(() => {
+    // this is the key line — fire unmute when this player unmutes
+    if (!muted) {
+      audioManager.notifyPlaying(id);
+      unmute(true);
+    } else {
+      mute(true);
+    }
+  }, [muted]);
 
   const handleTimeUpdate = useCallback((event: any) => {
     onTimeUpdate?.(event);
