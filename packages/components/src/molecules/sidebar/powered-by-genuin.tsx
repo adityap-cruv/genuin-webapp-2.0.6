9bc61524a98388d799595ce4b5735b4dfc837fa3
Himanshu Mendapara <himanshu@begenuin.com>
2025-07-02 11:48:22 +0000

    Merged in bugfix/powered-by-genuin-issue (pull request #1616)
    fix(GEN-5946): powered by genuin issue fixed
    
    * fix(GEN-5946): powered by genuin issue fixed
    
    
    Approved-by: Krunal Shah

--- packages/components/src/molecules/sidebar/powered-by-genuin.tsx
+++ packages/components/src/molecules/sidebar/powered-by-genuin.tsx
@@ -1,18 +1,19 @@
-
 import { Link } from "@genuin/components/molecules/link";
-import {GenuinLogo} from "@genuin/ui/icons"
+import { GenuinLogo } from "@genuin/ui/icons";
 
 export function PoweredByGenuin() {
   return (
     <div className="gencl:!hidden gencl:xl:!block gencl:border-t gencl:p-4 gencl:border-secondary-100 gencl:justify-self-end gencl:mt-auto">
       <div className="gencl:flex gencl:items-center gencl:px-3">
-        <p className="gencl:text-body-2-semi-bold gencl:text-secondary-600">Powered by</p>
+        <p className="gencl:text-body-2-semi-bold gencl:whitespace-nowrap gencl:text-secondary-600">
+          Powered by
+        </p>
         <Link
           href="https://begenuin.com?utm_source=web&utm_medium=sidebar"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <GenuinLogo  />
+          <GenuinLogo />
         </Link>
       </div>
     </div>
