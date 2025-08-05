a77cd5c21020c23b2c50d06d05d039963d3fc0f1
Dhairya Patel <dhairya.p@begenuin.com>
2025-08-05 05:49:23 +0000

    Merged in bugfix/tooltip-issues-check (pull request #1793)
    fix(GEN-6024 :  fix the issue of tooltip)
    
    * fix(GEN-6024 :  fix the issue of tooltip)
    
    * fix(GEN-6024 : remove delay)
    
    
    Approved-by: Krunal Shah

--- packages/components/src/molecules/actions/tooltip.tsx
+++ packages/components/src/molecules/actions/tooltip.tsx
@@ -66,8 +66,19 @@ export function TooltipAction({
         )}
         onClick={onClick}
         {...restProps}
+        asChild
       >
-        {icon}
+        <div
+          className={cn(
+            tooltipVariants({ variant }),
+            "gencl:hover:cursor-pointer ",
+            "gencl:h-12 gencl:p-0 gencl:w-12 gencl:flex gencl:items-center gencl:justify-center  gencl:rounded-full",
+            "gencl:[&_svg]:w-8 gencl:[&_svg]:h-8",
+            className
+          )}
+        >
+          {icon}
+        </div>
       </TooltipTrigger>
       <TooltipContent theme={variant ?? "light"} side="right">
         <p>{tooltipText}</p>
