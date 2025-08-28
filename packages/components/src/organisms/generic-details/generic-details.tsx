ef502649ce8a1aa5d3e3aea97757f8b12b00488c
Himanshu Mendapara <himanshu@begenuin.com>
2025-08-28 12:57:40 +0530

    refactor(GEN-6339): update import paths for ReadMore component in Embed and GenericDetails

--- packages/components/src/organisms/generic-details/generic-details.tsx
+++ packages/components/src/organisms/generic-details/generic-details.tsx
@@ -5,7 +5,8 @@ import { useDeviceDetectMediaQuery } from "@genuin/components/hooks/use-devide-d
 import { GenericDetailsMobile } from "./generic-details-mobile";
 import { PinIcon } from "@genuin/ui/icons";
 import { cn } from "@genuin/ui/lib/utils";
-import { Avatar, ReadMore } from "@genuin/ui/components";
+import { Avatar } from "@genuin/ui/components";
+import { ReadMore } from "@genuin/components/molecules/read-more";
 
 /**
  * Props for the GenericDetails component.
