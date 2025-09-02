7015eee744f24aff99592873617a5fc57e291355
Himanshu Mendapara <himanshu@begenuin.com>
2025-09-02 12:02:08 +0000

    Merged in bugfix/build-issue (pull request #1867)
    fix(GEN-6339): build issue fixed
    
    * fix(GEN-6339): build issue fixed
    
    
    Approved-by: Krunal Shah

--- packages/components/src/page/profile-details/profile-details.tsx
+++ packages/components/src/page/profile-details/profile-details.tsx
@@ -33,7 +33,6 @@ export function ProfileDetails({
   } = useGetProfileDetails(userName, forBrand);
   const detailsId = useId();
   const { brandDetails } = useBaseContext();
-  const { isMobile } = useDeviceDetectMediaQuery();
   const { replace } = useRouter();
   const { isMobile } = useDeviceDetectMediaQuery();
   const { createExternalLink } = useLinkContext();
@@ -60,7 +59,7 @@ export function ProfileDetails({
 
     if (error.message === NOT_FOUND_ERROR_CODES.brand) {
       return <ErrorState type="NO_BRAND_USER" />;
-    } 
+    }
 
     return <ErrorState type="ERROR" />;
   }
