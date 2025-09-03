8e7706b4c382f9b7f908ba5891c8563bb445ba88
krunals3 <krunal.s@begenuin.com>
2025-09-03 18:11:23 +0530

    GEN-6603: Brand/profile page issue resolved

--- packages/components/src/page/profile-details/profile-details.tsx
+++ packages/components/src/page/profile-details/profile-details.tsx
@@ -33,6 +33,7 @@ export function ProfileDetails({
   } = useGetProfileDetails(userName, forBrand);
   const detailsId = useId();
   const { brandDetails } = useBaseContext();
+  const { isMobile } = useDeviceDetectMediaQuery();
   const { replace } = useRouter();
   const { createExternalLink } = useLinkContext();
 
