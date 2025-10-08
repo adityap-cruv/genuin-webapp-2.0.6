ad501874e9c23406d37f8a2ea81f60d7b7280651
Dhairya Patel <dhairya.p@begenuin.com>
2025-10-08 07:25:03 +0000

    Merged in bugfix/ted-fourth (pull request #2046)
    fix(GEN-6024 : fix the issue of ted)
    
    * fix(GEN-6024 : fix the issue of ted)
    
    
    Approved-by: Jimit Shah

--- packages/components/src/molecules/mention-input/highlighted-field.tsx
+++ packages/components/src/molecules/mention-input/highlighted-field.tsx
@@ -157,7 +157,7 @@ const HighlightedInput = forwardRef<
             <div
               ref={highlightRef}
               className={cn(
-                "gencl:absolute gencl:top-0 gencl:text-body-0-medium gencl:left-0 gencl:pointer-events-none gencl:overflow-auto gencl:whitespace-pre-wrap gencl:z-0 gencl:border gencl:border-transparent gencl:rounded-md",
+                "gencl:absolute gencl:top-0 gencl:text-body-0-medium gencl:left-0 gencl:pointer-events-none gencl:overflow-auto gencl:whitespace-pre-wrap gencl:z-0 gencl:border gencl:border-transparent gencl:rounded-md gencl:w-full",
                 classes.highlight
               )}
               style={{
