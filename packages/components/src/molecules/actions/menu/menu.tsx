c080cb9264515a0acfc4d9f8d795410e4a095cfd
Dhairya Patel <dhairya.p@begenuin.com>
2025-10-15 12:33:37 +0000

    Merged in bugfix/added-class-popover (pull request #2096)
    fix(GEN-6024 : added class in popover)
    
    * fix(GEN-6024 : added class in popover)
    
    
    Approved-by: Krunal Shah

--- packages/components/src/molecules/actions/menu/menu.tsx
+++ packages/components/src/molecules/actions/menu/menu.tsx
@@ -162,7 +162,7 @@ export function Menu({
       <PopoverTrigger asChild>{children}</PopoverTrigger>
       <PopoverContent
         align="start"
-        className="gen-sdk-class gencl:w-fit gencl:p-3 gencl:border gencl:border-secondary-100 gencl:rounded-xl gencl:flex gencl:flex-col gencl:gap-0.5 gencl:z-50 gencl:!bg-white gencl:focus-visible:outline-none gencl:focus-visible:ring-0"
+        className="gen-sdk-class gen-sdk-expand-view gencl:w-fit gencl:p-3 gencl:border gencl:border-secondary-100 gencl:rounded-xl gencl:flex gencl:flex-col gencl:gap-0.5 gencl:z-50 gencl:!bg-white gencl:focus-visible:outline-none gencl:focus-visible:ring-0"
       >
         {MenuData.map((data, index) => (
           <React.Fragment key={index}>{data.children}</React.Fragment>
