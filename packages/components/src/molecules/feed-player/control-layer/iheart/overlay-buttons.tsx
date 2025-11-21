63a40aafbc129d69821410ce19442842e0706bd5
Dhairya Patel <dhairya.p@begenuin.com>
2025-11-21 07:14:05 +0000

    Merged in bugfix/overlay-button-width (pull request #2260)
    Bugfix/overlay button width
    
    * fix(GEN-6024 : width issue fixed)
    
    * fix(GEN-6024 : fix the issue of the width)
    
    * fix(GEN-6024 : remove the code)
    
    
    Approved-by: Krunal Shah

--- packages/components/src/molecules/feed-player/control-layer/iheart/overlay-buttons.tsx
+++ packages/components/src/molecules/feed-player/control-layer/iheart/overlay-buttons.tsx
@@ -50,7 +50,7 @@ export function OverLayButton({
       onClick={isClickDisabled ? undefined : (event) => handleClick(event)}
       aria-label="Go to all episodes page"
       className={cn(
-        "gencl:h-11 gencl:text-body-1-semi-bold! gencl:flex gencl:items-center gencl:justify-center gencl:rounded-full gencl:px-5 gencl:size-8!",
+        "gencl:text-body-1-semi-bold! gencl:flex gencl:items-center gencl:justify-center gencl:rounded-full gencl:px-5 gencl:h-8",
         isPlaying
           ? "gencl:bg-transparent gencl:text-white gencl:border! gencl:border-white!"
           : "gencl:bg-white gencl:text-[#27292D]!",
