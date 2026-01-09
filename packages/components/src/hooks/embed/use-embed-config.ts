a75042bebba640d8a69018de7f5f025686e88ed4
Dev Tejot <devtejot@begenuin.com>
2026-01-09 11:14:22 +0000

    Merged in bugfix/GEN-7016/aspect-fix (pull request #2467)
    fix(GEN-7269: aspect-ration fix)
    
    * fix(GEN-7269: aspect-ration fix)
    
    
    Approved-by: Krunal Shah

--- packages/components/src/hooks/embed/use-embed-config.ts
+++ packages/components/src/hooks/embed/use-embed-config.ts
@@ -131,12 +131,10 @@ export function useEmbedConfigs() {
         : !!customization?.autoplay,
       moveToNextTime: embedData?.media_play?.auto_advance_playback ?? 0,
       showBorderAroundVideo:
-        (!!customization?.is_enable_engagement_tools ||
+        (!!customization?.is_show_social_interaction_data ||
           (customization?.links?.is_show_links &&
             customization?.links?.position === "outside")) &&
-        brandLayoutType !== "iheart"
-          ? true
-          : false,
+        brandLayoutType !== "iheart",
       videoCrop: customization?.video_crop,
       autoScrollToNextSlide: customization?.enable_auto_scroll ?? false,
       resumePlaybackFrom: brandDetails.web_configs.resume_playback_from,
