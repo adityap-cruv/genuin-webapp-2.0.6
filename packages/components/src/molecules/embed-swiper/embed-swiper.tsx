6d25005e5aaba5b835ef465fecea6b5d807ddcff
Himanshu Mendapara <himanshu@begenuin.com>
2026-03-17 12:39:32 +0000

    Merged in bugfix/scroll-related-issue-in-205 (pull request #2680)
    fix(GEN-8273): scroll issues in 205
    
    * fix(GEN-8273): scroll issues in 205
    
    
    Approved-by: Krunal Shah

--- packages/components/src/molecules/embed-swiper/embed-swiper.tsx
+++ packages/components/src/molecules/embed-swiper/embed-swiper.tsx
@@ -34,7 +34,7 @@ const SwiperWithModules = lazy(async () => {
       ...props
     }: {
       children: React.ReactNode;
-      freeMode?: boolean;
+      freeMode?: boolean | object;
       virtualizeSwiper?: boolean;
       [key: string]: any;
     }) => {
@@ -189,7 +189,7 @@ export function EmbedSwiper({
         }}
         spaceBetween={spaceBetweenVideos}
         speed={SWIPER_CONFIG.SCROLL_DELAY}
-        freeMode={freeMode}
+        freeMode={freeMode ? SWIPER_CONFIG.FREE_MODE : false}
         virtualizeSwiper={virtualizeSwiper}
         watchOverflow={true}
         keyboard={{
